355d67b3a0c7bcf0b8693e78586b1fd5b3e3933d
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2017-12-07 22:36:11 -0500

    Minor cleanup.

--- cpp/tests/navigation.cpp
+++ cpp/tests/navigation.cpp
@@ -78,32 +78,15 @@ poly_fem::Navigation::Index poly_fem::Navigation::switch_edge(const GEO::Mesh &M
 	if (v2 == (index_t) idx.v) {
 		v2 = M.edges.vertex(idx.e, 1);
 	}
-	//std::cout << M.edges.vertex(idx.e, 0) << ' ' << M.edges.vertex(idx.e, 1) << std::endl;
 	GEO::Attribute<index_t> c2e(M.facet_corners.attributes(), "edge_id");
 	index_t c1 = M.facets.next_corner_around_facet(idx.f, idx.fc);
 	index_t v1 = M.facet_corners.vertex(c1);
-	// auto show_corner = [&] (int c) {
-	// 	index_t v = M.facet_corners.vertex(c);
-	// 	index_t e = c2e[c];
-	// 	const index_t *ve = M.edges.vertex_index_ptr(e);
-	// 	std::cout << "c: " << c << " v: " << v << " e: " << e << " (" << ve[0] << "," << ve[1] << ")" << std::endl;
-	// };
-	// const index_t *ve = M.edges.vertex_index_ptr(idx.e);
-	// std::cout << idx.v << ' ' << idx.e << " (" << ve[0] << ',' << ve[1] << ")" << std::endl;
-	// show_corner(idx.fc);
-	// show_corner(M.facets.next_corner_around_facet(idx.f, idx.fc));
-	// show_corner(M.facets.prev_corner_around_facet(idx.f, idx.fc));
-	index_t c3 = M.facets.prev_corner_around_facet(idx.f, idx.fc);
-	index_t v3 = M.facet_corners.vertex(c3);
-	//std::cout << v1 << ' ' << idx.v << ' ' << v2 << std::endl;
 	if (v1 == v2) {
 		index_t c2 = M.facets.prev_corner_around_facet(idx.f, idx.fc);
 		idx.e = c2e[c2];
-		// std::cout << idx.e << std::endl;
 		return idx;
 	} else {
 		idx.e = c2e[idx.fc];
-		// std::cout << idx.e << std::endl;
 		return idx;
 	}
 }
@@ -121,6 +104,8 @@ poly_fem::Navigation::Index poly_fem::Navigation::switch_face(const GEO::Mesh &M
 		idx.f = -1;
 		return idx;
 	} else {
+		// Iterate over all corners of the new face until we find the vertex we came from.
+		// Not ideal but for now it will do the job.
 		for (index_t c2 = M.facets.corners_begin(f2); c2 < M.facets.corners_end(f2); ++c2) {
 			index_t v2 = M.facet_corners.vertex(c2);
 			if (v2 == (index_t) idx.v) {
