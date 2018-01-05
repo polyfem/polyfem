961ff8721decb2280cfa52bb7e81ddbd3d7b36ec
Teseo Schneider <teseo@bluewin.ch>
2018-01-04 22:39:00 -0500

    fixed cmake of mkl

--- cpp/cmake/FindMKL.cmake
+++ cpp/cmake/FindMKL.cmake
@@ -7,62 +7,33 @@
 #
 ################################################################################
 
-if(MKL_LIBRARIES)
+if(DEFINED MKL_LIBRARIES)
 	set(MKL_FIND_QUIETLY TRUE)
 endif()
 
 if(CMAKE_MINOR_VERSION GREATER 4)
 	if(${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
-		find_library(MKL_LIBRARIES
-				mkl_core
-			PATHS
-				$ENV{MKLLIB}
-				/opt/intel/mkl/*/lib/em64t
-				/opt/intel/mkl/lib/intel64
-				/opt/intel/Compiler/*/*/mkl/lib/em64t
-				${LIB_INSTALL_DIR})
-
-		find_library(MKL_GUIDE
-				guide
-			PATHS
-				$ENV{MKLLIB}
-				/opt/intel/mkl/*/lib/em64t
-				/opt/intel/Compiler/*/*/mkl/lib/em64t
-				/opt/intel/Compiler/*/*/lib/intel64
-				${LIB_INSTALL_DIR})
-
-		if(MKL_LIBRARIES AND MKL_GUIDE)
-			set(MKL_LIBRARIES ${MKL_LIBRARIES} mkl_intel_lp64 mkl_sequential ${MKL_GUIDE} pthread)
-		else()
-			set(MKL_LIBRARIES ${MKL_LIBRARIES} mkl_intel_lp64 mkl_sequential pthread)
-		endif()
-
+		set(MKL_LIBS mkl_core guide mkl_intel_lp64 mkl_sequential pthread)
 	else()
-		find_library(MKL_LIBRARIES
-				mkl_core
-			PATHS
-				$ENV{MKLLIB}
-				/opt/intel/mkl/*/lib/32
-				/opt/intel/Compiler/*/*/mkl/lib/32
-				${LIB_INSTALL_DIR})
-
-		find_library(MKL_GUIDE
-				guide
-			PATHS
-				$ENV{MKLLIB}
-				/opt/intel/mkl/*/lib/32
-				/opt/intel/Compiler/*/*/mkl/lib/32
-				/opt/intel/Compiler/*/*/lib/intel32
-				${LIB_INSTALL_DIR})
-
-		if(MKL_LIBRARIES AND MKL_GUIDE)
-			set(MKL_LIBRARIES ${MKL_LIBRARIES} mkl_intel mkl_sequential ${MKL_GUIDE} pthread)
-		else()
-			set(MKL_LIBRARIES ${MKL_LIBRARIES} mkl_intel mkl_sequential pthread)
-		endif()
-
+		set(MKL_LIBS mkl_core guide mkl_intel mkl_sequential pthread)
 	endif()
 endif()
+
+foreach(LIB ${MKL_LIBS})
+	find_library(TMP
+			${LIB}
+		PATHS
+			$ENV{MKLLIB}
+			/opt/intel/mkl/*/lib/em64t
+			/opt/intel/mkl/lib/intel64
+			/opt/intel/Compiler/*/*/mkl/lib/em64t
+			${LIB_INSTALL_DIR})
+
+	if(TMP)
+		set(MKL_LIBRARIES ${MKL_LIBRARIES} ${TMP})
+	endif()
+
+endforeach()
 
 find_path(MKL_INCLUDE_DIR
 		mkl.h
