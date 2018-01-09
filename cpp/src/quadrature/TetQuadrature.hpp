9a3d94bea4e0e3194582165dfe6b9c204ed58874
Teseo Schneider <teseo@bluewin.ch>
2018-01-09 11:44:54 -0500

    added tet quadrature

---
+++ cpp/src/quadrature/TetQuadrature.hpp
@@ -0,0 +1,17 @@
+#ifndef TET_QUADRATURE_HPP
+#define TET_QUADRATURE_HPP
+
+#include "Quadrature.hpp"
+
+namespace poly_fem
+{
+    class TetQuadrature
+    {
+    public:
+        TetQuadrature();
+
+        void get_quadrature(const int order, Quadrature &quad);
+    };
+}
+
+#endif //TET_QUADRATURE_HPP
