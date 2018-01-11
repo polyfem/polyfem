30294a9c0bc02e7a6c752e22d0cd6e47f180ff68
Teseo Schneider <teseo@bluewin.ch>
2018-01-11 15:12:02 -0500

    missing file

---
+++ cpp/src/utils/Types.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace poly_fem
+{
+	// Stack-allocated vectors of size either 2 or 3
+	typedef Eigen::Matrix<double, Eigen::Dynamic, 1, 0, 3, 1> VectorNd;
+	typedef Eigen::Matrix<double, 1, Eigen::Dynamic, Eigen::RowMajor, 1, 3> RowVectorNd;
+}
