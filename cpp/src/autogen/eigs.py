7ee15d1b5400f80c1170867e57fef2b3701413c4
Teseo Schneider <teseo@bluewin.ch>
2018-03-28 11:21:32 -0400

    eigs in 2 and 3 working with autodiff

--- cpp/src/autogen/eigs.py
+++ cpp/src/autogen/eigs.py
@@ -1,11 +1,52 @@
 from sympy import *
 from sympy.matrices import *
-import re
 import os
+import re
 import argparse
 
 # local
 import pretty_print
+
+
+def sqr(a):
+    return a * a
+
+
+def trunc_acos(x):
+    tmp = Piecewise((0.0, x >= 1.0), (pi, x <= -1.0), (acos(x), True))
+    return tmp.subs(x, x)
+
+
+def eigs_3d(mat):
+    b = mat[0] + mat[4] + mat[8]
+    t = sqr(mat[1]) + sqr(mat[2]) + sqr(mat[5])
+    p = 0.5 * (sqr(mat[0] - mat[4]) + sqr(mat[0] - mat[8]) + sqr(mat[4] - mat[8]))
+    p += 3.0 * t
+    q = 18.0 * (mat[0] * mat[4] * mat[8] + 3.0 * mat[1] * mat[2] * mat[5])
+    q += 2.0 * (mat[0] * sqr(mat[0]) + mat[4] * sqr(mat[4]) + mat[8] * sqr(mat[8]))
+    q += 9.0 * b * t
+    q -= 3.0 * (mat[0] + mat[4]) * (mat[0] + mat[8]) * (mat[4] + mat[8])
+    q -= 27.0 * (mat[0] * sqr(mat[5]) + mat[4] * sqr(mat[2]) + mat[8] * sqr(mat[1]))
+
+    delta = trunc_acos(0.5 * q / sqrt(p * sqr(p)))
+    p = 2.0 * sqrt(p)
+
+    tmp1 = Piecewise(
+        (b / 3.0, p < 1e-10),
+        ((b + p * cos(delta / 3.0)) / 3.0, True)
+    )
+
+    tmp2 = Piecewise(
+        (b / 3.0, p < 1e-10),
+        ((b + p * cos((delta + 2.0 * pi) / 3.0)) / 3.0, True)
+    )
+
+    tmp3 = Piecewise(
+        (b / 3.0, p < 1e-10),
+        ((b + p * cos((delta - 2.0 * pi) / 3.0)) / 3.0, True)
+    )
+
+    return tmp1.subs(p, p), tmp2.subs(p, p), tmp3.subs(p, p)
 
 
 def parse_args():
@@ -22,13 +63,13 @@ if __name__ == "__main__":
     dims = [2, 3]
 
     cpp = "#include \"auto_eigs.hpp\"\n\n\n"
-    hpp = "#pragma once\n\n#include <complex>\n#include <Eigen/Dense>\n\n"
+    hpp = "#pragma once\n\n#include <Eigen/Dense>\n\n"
     cpp = cpp + "namespace poly_fem {\nnamespace autogen " + "{\n"
     hpp = hpp + "namespace poly_fem {\nnamespace autogen " + "{\n"
 
     hpp = hpp + "template<typename T>\nT int_pow(T val, int exp) { T res = exp <=0 ? T(0.): val; for(int i = 1; i < exp; ++i) res = res*val; return res; }\n\n"
 
-    lambdaa = Symbol('lambda')
+    lambdaa = Symbol('lambda', real=True)
 
     for dim in dims:
         print("processing " + str(dim))
@@ -38,57 +79,28 @@ if __name__ == "__main__":
         for i in range(0, dim):
             for j in range(0, dim):
                 if i <= j:
-                    M[i, j] = Symbol('m[' + str(i) + ',' + str(j) + ']')
+                    M[i, j] = Symbol('m[' + str(i) + ',' + str(j) + ']', real=True)
                 else:
-                    M[i, j] = Symbol('m[' + str(j) + ',' + str(i) + ']')
+                    M[i, j] = Symbol('m[' + str(j) + ',' + str(i) + ']', real=True)
 
-        # M = simplify(M * Transpose(M))
-        # print(M)
+        if dim == 2:
+            lambdas = M.eigenvals()
+        else:
+            lambdas = eigs_3d(M)
 
-        # if dim == 2:
-        #     lambdas = roots(det(lambdaa * eye(dim) - M), lambdaa)
-        # else:
-        #     lambdas = roots(det(lambdaa * eye(dim) - M), lambdaa)
-
-        # for i in range(0, dim):
-        #     lambdas[i] = real(lambdas[i])
-
-        lambdas = M.eigenvals()
-        lambdas = simplify(lambdas)
-        # print(lambdas)
+        # lambdas = simplify(lambdas)
 
         c99 = pretty_print.C99_print(lambdas)
-        c99 = re.sub("m\[(\d{1}),(\d{1})\]", r'm(\1,\2)', c99)
-        c99 = re.sub("result_(\d{1})", r'res(\1)', c99)
-        c99 = re.sub("I", "1i", c99)
-        c99 = re.sub("L", "", c99)
-        c99 = c99.replace(" 1;", " T(1.);")
-        c99 = c99.replace(" 2*", " T(2.)*")
-        c99 = c99.replace(" 3*", " T(3.)*")
-        c99 = c99.replace(" 4*", " T(4.)*")
-        c99 = c99.replace(" 27*", " T(27.)*")
-        c99 = c99.replace("-27*", "T(-27.)*")
-        c99 = c99.replace(" 9*", " T(9.)*")
-        c99 = c99.replace(" 54*", " T(54.)*")
-        c99 = c99.replace("-4*", "T(-4.)*")
-        c99 = c99.replace("27.0/2.0", "T(27.0/2.0)")
-        c99 = c99.replace("9.0/2.0", "T(9.0/2.0)")
-        c99 = c99.replace("1.0/2.0", "T(1.0/2.0)")
-        c99 = c99.replace("1.0/3.0", "T(1.0/3.0)")
 
-        c99 = c99.replace("pow", "int_pow")
-
-        if dim == 3:
-            c99 = re.sub("cbrt\((.*)\)", r"pow(\1,1./3.)", c99)
-
-            c99 = re.sub("auto", "std::complex<T>", c99)
-            c99 = re.sub("res\((\d)\) = (.*);", r'res(\1) = (\2).real();', c99)
+        c99 = re.sub(r"m\[(\d{1}),(\d{1})\]", r'm(\1,\2)', c99)
+        c99 = re.sub(r"result_(\d{1})", r'res(\1)', c99)
+        c99 = c99.replace("0.0", "T(0)")
+        c99 = c99.replace("   M_PI", "   T(M_PI)")
 
         signature = "template<typename T>\nvoid eigs_" + str(dim) + "d(const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, 0, 3, 3> &m, "
         signature += "Eigen::Matrix<T, Eigen::Dynamic, 1, 0, 3, 1> &res)"
 
-        hpp = hpp + signature + " {\nres.resize(" + str(dim) + ");\nusing namespace std::complex_literals;\n" + c99 + "\n}\n\n"
-        # hpp = hpp + signature + ";\n"
+        hpp = hpp + signature + " {\nres.resize(" + str(dim) + ");\n" + c99 + "\n}\n\n"
 
     cpp = cpp + "\n"
     hpp = hpp + "\n"
@@ -96,7 +108,6 @@ if __name__ == "__main__":
     cpp = cpp + "\n}}\n"
     hpp = hpp + "\n}}\n"
 
-    # print(cpp)
     path = os.path.abspath(args.output)
 
     print("saving...")
--- cpp/tests/test_matrix.cpp
+++ cpp/tests/test_matrix.cpp
@@ -69,6 +69,21 @@ TEST_CASE("eigs2autodiff", "[matrix]") {
     REQUIRE(actual_eigs(1).getValue() == e1);
 }
 
+TEST_CASE("eigs3id", "[matrix]") {
+    Eigen::Matrix<double, 3, 3> mat;
+    mat.setIdentity();
+
+   	Eigen::Matrix<double, Eigen::Dynamic, 1, 0, 3, 1> actual_eigs(3);
+   	autogen::eigs_3d<double>(mat, actual_eigs);
+
+   	const auto e0 = Approx(1).margin(1e-15);
+   	const auto e1 = Approx(1).margin(1e-15);
+   	const auto e2 = Approx(1).margin(1e-15);
+
+    REQUIRE(actual_eigs(0) == e0);
+    REQUIRE(actual_eigs(1) == e1);
+    REQUIRE(actual_eigs(2) == e2);
+}
 
 TEST_CASE("eigs3", "[matrix]") {
     Eigen::Matrix<double, 3, 3> mat;
@@ -81,9 +96,9 @@ TEST_CASE("eigs3", "[matrix]") {
    	autogen::eigs_3d<double>(mat, actual_eigs);
 
 
-   	const auto e0 = Approx(0.002128421644668).margin(1e-8);
-   	const auto e1 = Approx(0.366887383112958).margin(1e-8);
-   	const auto e2 = Approx(4.147907219689489).margin(1e-8);
+   	const auto e0 = Approx(4.147907219689489).margin(1e-8);
+   	const auto e1 = Approx(0.002128421644668).margin(1e-8);
+   	const auto e2 = Approx(0.366887383112958).margin(1e-8);
 
     REQUIRE(actual_eigs(0) == e0);
     REQUIRE(actual_eigs(1) == e1);
@@ -92,27 +107,27 @@ TEST_CASE("eigs3", "[matrix]") {
 
 
 TEST_CASE("eigs3autodiff", "[matrix]") {
-	// typedef DScalar2<double, Eigen::Matrix<double, 9, 1>, Eigen::Matrix<double, 9, 9>> T;
- //    typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, 0, 3, 3> 	AutoDiffGradMat;
- //    Eigen::Matrix<double, 3, 3> tmp;
- //    tmp <<
- //    	0.723799495850613,   0.738301489180688,   0.854652664143603,
- //   		0.738301489180688,   1.868383818516662,   1.880071527849840,
- //   		0.854652664143603,   1.880071527849840,   1.924739710079838;
+	typedef DScalar2<double, Eigen::Matrix<double, 9, 1>, Eigen::Matrix<double, 9, 9>> T;
+    typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, 0, 3, 3> 	AutoDiffGradMat;
+    Eigen::Matrix<double, 3, 3> tmp;
+    tmp <<
+    	0.723799495850613,   0.738301489180688,   0.854652664143603,
+   		0.738301489180688,   1.868383818516662,   1.880071527849840,
+   		0.854652664143603,   1.880071527849840,   1.924739710079838;
 
-		// DiffScalarBase::setVariableCount(9);
- //   	AutoDiffGradMat mat(3,3);
- //   	for(int i = 0; i < 9; ++i)
- //   		mat(i) = T(i, tmp(i));
+		DiffScalarBase::setVariableCount(9);
+   	AutoDiffGradMat mat(3,3);
+   	for(int i = 0; i < 9; ++i)
+   		mat(i) = T(i, tmp(i));
 
- //   	Eigen::Matrix<T, Eigen::Dynamic, 1, 0, 3, 1> actual_eigs(3);
- //   	autogen::eigs_3d<T>(mat, actual_eigs);
+   	Eigen::Matrix<T, Eigen::Dynamic, 1, 0, 3, 1> actual_eigs(3);
+   	autogen::eigs_3d<T>(mat, actual_eigs);
 
- //   	const auto e0 = Approx(0.002128421644668).margin(1e-8);
- //   	const auto e1 = Approx(0.366887383112958).margin(1e-8);
- //   	const auto e2 = Approx(4.147907219689489).margin(1e-8);
+	const auto e0 = Approx(4.147907219689489).margin(1e-8);
+	const auto e1 = Approx(0.002128421644668).margin(1e-8);
+	const auto e2 = Approx(0.366887383112958).margin(1e-8);
 
- //    REQUIRE(actual_eigs(0).getValue() == e0);
- //    REQUIRE(actual_eigs(1).getValue() == e1);
- //    REQUIRE(actual_eigs(2).getValue() == e2);
+    REQUIRE(actual_eigs(0).getValue() == e0);
+    REQUIRE(actual_eigs(1).getValue() == e1);
+    REQUIRE(actual_eigs(2).getValue() == e2);
 }
