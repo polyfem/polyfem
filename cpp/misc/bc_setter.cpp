b232715eebe8c523f7161def7c3109d87e4a2457
Teseo Schneider <teseo@bluewin.ch>
2018-06-15 11:35:00 -0400

    better ui for setting bc

--- cpp/misc/bc_setter.cpp
+++ cpp/misc/bc_setter.cpp
@@ -30,6 +30,8 @@ static const int BUF_SIZE = 128;
 
 igl::ColorMapType color_map = igl::COLOR_MAP_TYPE_VIRIDIS;
 
+static const ImGuiWindowFlags WINDOW_FLAGS = ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_AlwaysAutoResize;
+
 struct BCVals
 {
 	std::vector<std::array<float,3>> vals;
@@ -52,6 +54,11 @@ struct BCVals
 		vals.push_back(std::array<float,3>());
 		vals.back()[0]=vals.back()[1]=vals.back()[2] = 0;
 		Matrix<std::array<char,BUF_SIZE>, 3, 1> tmp;
+		for(int i = 0; i < 3; ++i)
+		{
+			for(int j = 0; j < BUF_SIZE; ++j)
+				tmp(i)[j] = 0;
+		}
 		funs.push_back(tmp);
 
 		bc_type.push_back(0);
@@ -75,7 +82,7 @@ RowVector3d color(int bc, int n_cols)
 	{
 		MatrixXd tmp;
 		MatrixXd v(1,1); v(0) = bc;
-		igl::colormap(color_map, v, 1, n_cols, tmp);
+		igl::colormap(color_map, v, 0, n_cols, tmp);
 		col = tmp;
 	}
 
@@ -239,83 +246,103 @@ int main(int argc, const char **argv)
 	igl::opengl::glfw::imgui::ImGuiMenu menu;
 	viewer.plugins.push_back(&menu);
 
-
-	menu.callback_draw_viewer_menu = [&]()
+	menu.callback_draw_viewer_window = []() { };
+	menu.callback_draw_viewer_menu = []() { };
+	menu.callback_draw_custom_window = [&]()
 	{
-		ImGui::PushItemWidth(ImGui::GetWindowWidth());
-		if(ImGui::Button("Load mesh")){
-			std::string fname = igl::file_dialog_open();
-
-			if (fname.length() == 0)
-				return;
-
-			load(fname, V, F, p0, p1, N, adj, selected, all_2_local, boundary_2_all, C);
-			visited.resize(F.rows());
-
-			vals.reset();
-
-
-			viewer.data().clear();
-			viewer.data().add_edges(p0, p1, RowVector3d(0,0,0));
-			viewer.data().set_mesh(V, F);
-			viewer.data().set_colors(C);
-			viewer.core.align_camera_center(V);
-		}
-		ImGui::SameLine();
-		if(ImGui::Button("Save boundary conditions"))
+		if (ImGui::BeginMainMenuBar())
 		{
-			std::string fname = igl::file_dialog_save();
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("Open", "Ctrl+O"))
+				{
+					std::string fname = igl::file_dialog_open();
 
-			if(fname.length() == 0)
-				return;
+					if (fname.length() == 0)
+						return;
 
-			save(fname, selected, vals);
+					load(fname, V, F, p0, p1, N, adj, selected, all_2_local, boundary_2_all, C);
+					visited.resize(F.rows());
+
+					vals.reset();
+
+
+					viewer.data().clear();
+					viewer.data().add_edges(p0, p1, RowVector3d(0,0,0));
+					viewer.data().set_mesh(V, F);
+					viewer.data().set_colors(C);
+					viewer.core.align_camera_center(V);
+				}
+				if (ImGui::MenuItem("Save", "Ctrl+S"))
+				{
+					std::string fname = igl::file_dialog_save();
+
+					if(fname.length() == 0)
+						return;
+
+					save(fname, selected, vals);
+				}
+				ImGui::EndMenu();
+			}
+			ImGui::EndMainMenuBar();
 		}
-		ImGui::PopItemWidth();
-		ImGui::Separator();
+
+		const float ui_scaling_factor = menu.hidpi_scaling() / menu.pixel_ratio();
+		const float menu_width = 180 * ui_scaling_factor;
+
+		ImGui::SetNextWindowPos(ImVec2(5,20), ImGuiSetCond_FirstUseEver);
+		ImGui::SetNextWindowSize(ImVec2(0.0f, 0.0f), ImGuiSetCond_FirstUseEver);
+  		ImGui::SetNextWindowSizeConstraints(ImVec2(menu_width, -1.0f), ImVec2(menu_width, -1.0f));
+
+		static bool show_file_menu = true;
+		ImGui::Begin("Boundary conditions", &show_file_menu, WINDOW_FLAGS );
 
 		ImGui::RadioButton("clear##bc_selector", &current_id, 0);
 		ImGui::Separator();
 		for(int i = 1; i <= int(vals.size()); ++i){
-			std::string label = std::to_string(i) + "##bc_selector";
-			ImGui::RadioButton(label.c_str(), &current_id, i);
-		}
-		ImGui::Separator();
-
-		for(int i = 1; i <= int(vals.size()); ++i){
 			std::string label = std::to_string(i);
+
+			std::string title = "Boundary " + label;
+			std::string rlabel = "##bc_selector";
+
 			std::string vlabel = "Value##idvalue" + label;
 			std::string flabel = "Function##idvalue" + label;
-			ImGui::RadioButton(vlabel.c_str(), &vals.bc_value[i-1], 0); ImGui::SameLine();
-			ImGui::RadioButton(flabel.c_str(), &vals.bc_value[i-1], 1);
 
-			if(vals.bc_value[i-1] == 0)
+			std::string dlabel = "Dirichlet##idtype" + label;
+			std::string nlabel = "Neuman##idtype" + label;
+
+			ImGui::TextColored(ImVec4(color(i, vals.size())(0),color(i, vals.size())(1),color(i, vals.size())(2),1.0f), "%s", title.c_str()); ImGui::SameLine();
+
+
+			ImGui::RadioButton(rlabel.c_str(), &current_id, i);
+
+			//if (ImGui::CollapsingHeader("BC", ImGuiWindowFlags_NoCollapse))
 			{
-				ImGui::PushItemWidth(ImGui::GetWindowWidth() * 0.80f);
-				ImGui::InputFloat3(label.c_str(), vals.vals[i-1].data());
-			}
-			else
-			{
+				ImGui::RadioButton(vlabel.c_str(), &vals.bc_value[i-1], 0); ImGui::SameLine();
+				ImGui::RadioButton(flabel.c_str(), &vals.bc_value[i-1], 1);
+
 				ImGui::PushItemWidth(ImGui::GetWindowWidth() * 0.20f);
-				ImGui::InputText("x", vals.funs[i-1](0).data(), BUF_SIZE);  ImGui::SameLine();
-				ImGui::InputText("y", vals.funs[i-1](1).data(), BUF_SIZE);  ImGui::SameLine();
-				ImGui::InputText("z", vals.funs[i-1](2).data(), BUF_SIZE);
+
+				if(vals.bc_value[i-1] == 0)
+				{
+					ImGui::InputFloat("x", &vals.vals[i-1][0], 0,0, 3);  ImGui::SameLine();
+					ImGui::InputFloat("y", &vals.vals[i-1][1], 0,0, 3);  ImGui::SameLine();
+					ImGui::InputFloat("z", &vals.vals[i-1][2], 0,0, 3);
+				}
+				else
+				{
+					ImGui::InputText("x", vals.funs[i-1](0).data(), BUF_SIZE);  ImGui::SameLine();
+					ImGui::InputText("y", vals.funs[i-1](1).data(), BUF_SIZE);  ImGui::SameLine();
+					ImGui::InputText("z", vals.funs[i-1](2).data(), BUF_SIZE);
+				}
 				ImGui::PopItemWidth();
+
+				ImGui::RadioButton(dlabel.c_str(), &vals.bc_type[i-1], 0); ImGui::SameLine();
+				ImGui::RadioButton(nlabel.c_str(), &vals.bc_type[i-1], 1);
 			}
+
+			ImGui::Separator();
 		}
-		ImGui::Separator();
-
-
-		for(int i = 1; i <= int(vals.size()); ++i){
-			std::string label = std::to_string(i);
-			std::string dlabel = "Dirichlet##id" + label;
-			std::string nlabel = "Neuman##id" + label;
-
-			ImGui::TextColored(ImVec4(color(i, vals.size())(0),color(i, vals.size())(1),color(i, vals.size())(2),1.0f), "%s", label.c_str()); ImGui::SameLine();
-			ImGui::RadioButton(dlabel.c_str(), &vals.bc_type[i-1], 0); ImGui::SameLine();
-			ImGui::RadioButton(nlabel.c_str(), &vals.bc_type[i-1], 1);
-		}
-		ImGui::Separator();
 
 		if(ImGui::Button("Add ID"))
 		{
@@ -332,6 +359,8 @@ int main(int argc, const char **argv)
 			current_id = vals.size();
 			viewer.data().set_colors(C);
 		}
+
+		ImGui::End();
 	};
 
 	auto paint = [&]() {
