33087a20633c4d7b57eb4c7086a0418ddb000f68
Teseo Schneider <teseo@bluewin.ch>
2018-10-15 17:35:56 -0400

    updated libigl

--- cpp/misc/bc_setter.cpp
+++ cpp/misc/bc_setter.cpp
@@ -371,7 +371,7 @@ int main(int argc, const char **argv)
 		// Cast a ray in the view direction starting from the mouse position
 		double x = viewer.current_mouse_x;
 		double y = viewer.core.viewport(3) - viewer.current_mouse_y;
-		if(igl::unproject_onto_mesh(Vector2f(x,y), viewer.core.view * viewer.core.model,
+		if(igl::unproject_onto_mesh(Vector2f(x,y), viewer.core.view,
 			viewer.core.proj, viewer.core.viewport, V, F, fid, bc))
 		{
 
