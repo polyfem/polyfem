57b136858b8ee313d4da35d87edf6bcbc9098124
Teseo Schneider <teseo@bluewin.ch>
2018-10-31 17:14:23 -0400

    changed log level

--- cpp/src/assembler/RhsAssembler.cpp
+++ cpp/src/assembler/RhsAssembler.cpp
@@ -301,7 +301,7 @@ namespace polyfem
 			for(long i = 0; i < b.cols(); ++i){
 				solver->solve(b.col(i), coeffs.col(i));
 			}
-			logger().debug("RHS solve error {}", (A*coeffs-b).norm());
+			logger().trace("RHS solve error {}", (A*coeffs-b).norm());
 
 			for(long i = 0; i < coeffs.rows(); ++i){
 				for(int d = 0; d < size_; ++d){
