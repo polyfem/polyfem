79a619acc23d4c2dfbfe9b6d55e30caaf1c14460
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2019-01-22 21:17:14 -0500

    Fix bug in von mises formula.

--- src/utils/ElasticityUtils.cpp
+++ src/utils/ElasticityUtils.cpp
@@ -245,13 +245,15 @@ namespace polyfem
 	double von_mises_stress_for_stress_tensor(const Eigen::MatrixXd &stress)
 	{
 		//TODO???
-		double von_mises_stress = ( stress(0, 0) - stress(1, 1) ) * ( stress(0, 0) - stress(1, 1) ) + 3.0  *  stress(0, 1) * stress(1, 0);
+		double von_mises_stress;
 
 		if(stress.rows() == 3)
 		{
-			von_mises_stress *= 0.5;
+			von_mises_stress  = 0.5 * (stress(0, 0) - stress(1, 1)) * (stress(0, 0) - stress(1, 1)) + 3.0  * stress(0, 1) * stress(1, 0);
 			von_mises_stress += 0.5 * (stress(2, 2) - stress(1, 1)) * (stress(2, 2) - stress(1, 1)) + 3.0  * stress(2, 1) * stress(2, 1);
 			von_mises_stress += 0.5 * (stress(2, 2) - stress(0, 0)) * (stress(2, 2) - stress(0, 0)) + 3.0  * stress(2, 0) * stress(2, 0);
+		} else {
+			von_mises_stress = ( stress(0, 0) - stress(1, 1) ) * ( stress(0, 0) - stress(1, 1) ) + 3.0  *  stress(0, 1) * stress(1, 0);
 		}
 
 		von_mises_stress = sqrt( fabs(von_mises_stress) );
