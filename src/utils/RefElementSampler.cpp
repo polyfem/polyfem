7d35962a9123d5dd287b8ac34fddb6038e93b464
Teseo Schneider <teseo@bluewin.ch>
2019-06-11 15:57:59 -0400

    removed triangle from RefElementSampler.cpp, polygon needs to be subdivided

--- src/utils/RefElementSampler.cpp
+++ src/utils/RefElementSampler.cpp
@@ -1,111 +1,107 @@
 #include <polyfem/RefElementSampler.hpp>
 #include <polyfem/MeshUtils.hpp>
 
-#include <igl/triangle/triangulate.h>
 #include <igl/copyleft/tetgen/tetrahedralize.h>
 #include <igl/avg_edge_length.h>
 #include <igl/edges.h>
 #include <igl/write_triangle_mesh.h>
 
+namespace polyfem
+{
 
-namespace polyfem {
-
-namespace {
-
+namespace
+{
 ///
-/// Generate a canonical triangle subdivided into smaller triangles with the
-/// target area. This also ensures that boundary is periodic (boundary edges are
-/// evenly subdivided).
+/// Generate a canonical triangle subdivided fomr a regular grid
 ///
-/// @param[in]  target_area  { Target triangle area }
+/// @param[in]  n  			 { n grid quads }
+/// @param[in]  tri			 { is a tri or a quad }
 /// @param[out] OV           { #V x 2 output vertices positions }
 /// @param[out] OF           { #F x 3 output triangle indices }
 ///
-void triangulate_periodic(double target_area, Eigen::MatrixXd &OV, Eigen::MatrixXi &OF, Eigen::MatrixXi &OE) {
-	std::stringstream buf;
-	buf.precision(100);
-	buf.setf(std::ios::fixed, std::ios::floatfield);
-	buf << "Qqa" << target_area;
+void regular_2d_grid(const int n, bool tri, Eigen::MatrixXd &V, Eigen::MatrixXi &F)
+{
 
-	// Equilateral triangle
-	Eigen::MatrixXd V(3, 2);
-	V << 0, 0,
-		1, 0,
-		0.5, 0.5 * std::sqrt(3.0);
+	V.resize(n * n, 2);
+	F.resize((n - 1) * (n - 1) * 2, 3);
+	double delta = 1. / (n - 1.);
+	std::vector<int> map(n * n, -1);
 
-	Eigen::MatrixXi E(3, 2);
-	E << 0, 1,
-		1, 2,
-		2, 0;
-
-	// 1st pass gets an rough idea
-	igl::triangle::triangulate(V, E, Eigen::MatrixXd(0,2), buf.str(), OV, OF);
-
-	// Extract average edge-length, use it to sample the boundary evenly
-	double avg_len = igl::avg_edge_length(OV, OF);
-	int n = 1.0 / avg_len;
-
-	// Build subdivided triangle boundary
-	Eigen::MatrixXd V2(3 * n, 2);
-	Eigen::MatrixXi E2(3 * n, 2);
-	int rows = 3 * n;
-	int cols = 2;
-	E2 = Eigen::VectorXi::LinSpaced(rows, 0.0, rows - 1).replicate(1, cols);
-	E2.col(1) = E2.col(1).unaryExpr([&](const int x) { return (x+1)%rows; });
-	for (int d = 0; d < V2.cols(); ++d) {
-		V2.topRows(n).col(d) = Eigen::VectorXd::LinSpaced(n + 1, V(0, d), V(1, d)).head(n);
-		V2.middleRows(n, n).col(d) = Eigen::VectorXd::LinSpaced(n + 1, V(1, d), V(2, d)).head(n);
-		V2.bottomRows(n).col(d) = Eigen::VectorXd::LinSpaced(n + 1, V(2, d), V(0, d)).head(n);
+	int index = 0;
+	for(int i=0; i < n; ++i)
+	{
+    	for(int j = 0; j < n; ++j)
+		{
+        	if(tri && i+j >= n)
+            	continue;
+        map[i*n+j]=index;
+		V.row(index) << i * delta, j * delta;
+		++index;
+		}
 	}
 
-	// 2nd pass disable Steiner points on the boundary
-	buf << "Y";
-	igl::triangle::triangulate(V2, E2, Eigen::MatrixXd(0,2), buf.str(), OV, OF);
-	OE = E2;
+	V.conservativeResize(index, 2);
 
-	// Warp vertex positions to map them back to the canonical element
-	Eigen::Matrix2d M, Minv;
-	M << 1, 0,
-		0.5, 0.5 * std::sqrt(3.0);
-	Minv = M.inverse();
-	OV = OV * Minv;
+	std::array<int, 3> tmp;
+
+	index = 0;
+	for(int i=0; i < n-1; ++i)
+	{
+    	for(int j = 0; j < n-1; ++j)
+		{
+        	tmp = {{ map[i + j*n], map[i+1 + j*n], map[i + (j+1)*n] }};
+			if(tmp[0] >= 0 && tmp[1] >= 0 && tmp[2] >= 0)
+			{
+				F.row(index) << tmp[0], tmp[1], tmp[2];
+				++index;
+			}
+
+			tmp = {{ map[ i + 1 + j * n], map[i + 1 + (j + 1) * n], map[i + (j + 1) * n ] }};
+			if (tmp[0] >= 0 && tmp[1] >= 0 && tmp[2] >= 0) {
+				F.row(index) << tmp[0], tmp[1], tmp[2];
+				++index;
+			}
+		}
+	}
+
+	F.conservativeResize(index, 3);
 }
 
 } // anonymous namespace
 
-	RefElementSampler &RefElementSampler::sampler()
-	{
-		static RefElementSampler instance;
+RefElementSampler &RefElementSampler::sampler()
+{
+	static RefElementSampler instance;
 
-		return instance;
-	}
+	return instance;
+}
 
-	void RefElementSampler::init(const bool is_volume, const int n_elements, double target_rel_area)
-	{
-		is_volume_ = is_volume;
+void RefElementSampler::init(const bool is_volume, const int n_elements, double target_rel_area)
+{
+	is_volume_ = is_volume;
 
-		area_param_ = target_rel_area * n_elements;
+	area_param_ = target_rel_area * n_elements;
 #ifndef NDEBUG
-		area_param_ *= 10.0;
+	area_param_ *= 10.0;
 #endif
 
-		build();
-	}
+	build();
+}
 
-	void RefElementSampler::build()
+void RefElementSampler::build()
+{
+	using namespace Eigen;
+
+	std::stringstream buf;
+	buf.precision(100);
+	buf.setf(std::ios::fixed, std::ios::floatfield);
+
+	if (is_volume_)
 	{
-		using namespace Eigen;
-
-		std::stringstream buf;
-		buf.precision(100);
-		buf.setf(std::ios::fixed, std::ios::floatfield);
-
-		if(is_volume_)
+		buf << "Qpq1.414a" << area_param_;
 		{
-			buf<<"Qpq1.414a"<<area_param_;
-			{
-				MatrixXd pts(8,3); pts <<
-				0, 0, 0,
+			MatrixXd pts(8, 3);
+			pts << 0, 0, 0,
 				0, 1, 0,
 				1, 1, 0,
 				1, 0, 0,
@@ -116,8 +112,8 @@ void triangulate_periodic(double target_area, Eigen::MatrixXd &OV, Eigen::Matrix
 				1, 1, 1,
 				1, 0, 1;
 
-				Eigen::MatrixXi faces(12,3); faces <<
-				1, 2, 0,
+			Eigen::MatrixXi faces(12, 3);
+			faces << 1, 2, 0,
 				0, 2, 3,
 
 				5, 4, 6,
@@ -135,11 +131,11 @@ void triangulate_periodic(double target_area, Eigen::MatrixXd &OV, Eigen::Matrix
 				0, 3, 7,
 				0, 7, 4;
 
-				igl::copyleft::tetgen::tetrahedralize(pts, faces, buf.str(), cube_points_, cube_tets_, cube_faces_);
+			igl::copyleft::tetgen::tetrahedralize(pts, faces, buf.str(), cube_points_, cube_tets_, cube_faces_);
 
-				// Extract sampled edges matching the base element edges
-				Eigen::MatrixXi edges(12, 2); edges <<
-				0, 1,
+			// Extract sampled edges matching the base element edges
+			Eigen::MatrixXi edges(12, 2);
+			edges << 0, 1,
 				1, 2,
 				2, 3,
 				3, 0,
@@ -153,119 +149,135 @@ void triangulate_periodic(double target_area, Eigen::MatrixXd &OV, Eigen::Matrix
 				1, 5,
 				2, 6,
 				3, 7;
-				igl::edges(cube_faces_, cube_edges_);
-				extract_parent_edges(cube_points_, cube_edges_, pts, edges, cube_edges_);
+			igl::edges(cube_faces_, cube_edges_);
+			extract_parent_edges(cube_points_, cube_edges_, pts, edges, cube_edges_);
 
-				// Same local order as in FEMBasis3d
-				cube_corners_.resize(8, 3);
-				cube_corners_ <<
-					0, 0, 0,
-					1, 0, 0,
-					1, 1, 0,
-					0, 1, 0,
-					0, 0, 1,
-					1, 0, 1,
-					1, 1, 1,
-					0, 1, 1;
-			}
-			{
-				MatrixXd pts(4,3); pts <<
-				0, 0, 0,
+			// Same local order as in FEMBasis3d
+			cube_corners_.resize(8, 3);
+			cube_corners_ << 0, 0, 0,
+				1, 0, 0,
+				1, 1, 0,
+				0, 1, 0,
+				0, 0, 1,
+				1, 0, 1,
+				1, 1, 1,
+				0, 1, 1;
+		}
+		{
+			MatrixXd pts(4, 3);
+			pts << 0, 0, 0,
 				1, 0, 0,
 				0, 1, 0,
 				0, 0, 1;
 
-				Eigen::MatrixXi faces(4,3); faces <<
-				0, 1, 2,
+			Eigen::MatrixXi faces(4, 3);
+			faces << 0, 1, 2,
 
 				3, 1, 0,
 				2, 1, 3,
 				0, 2, 3;
 
-				MatrixXi tets;
-				igl::copyleft::tetgen::tetrahedralize(pts, faces, buf.str(), simplex_points_, simplex_tets_, simplex_faces_);
+			MatrixXi tets;
+			igl::copyleft::tetgen::tetrahedralize(pts, faces, buf.str(), simplex_points_, simplex_tets_, simplex_faces_);
 
-				// Extract sampled edges matching the base element edges
-				Eigen::MatrixXi edges;
-				igl::edges(faces, edges);
-				igl::edges(simplex_faces_, simplex_edges_);
-				extract_parent_edges(simplex_points_, simplex_edges_, pts, edges, simplex_edges_);
+			// Extract sampled edges matching the base element edges
+			Eigen::MatrixXi edges;
+			igl::edges(faces, edges);
+			igl::edges(simplex_faces_, simplex_edges_);
+			extract_parent_edges(simplex_points_, simplex_edges_, pts, edges, simplex_edges_);
 
-				// Same local order as in FEMBasis3d
-				simplex_corners_.resize(4, 3);
-				simplex_corners_ <<
-					0, 0, 0,
-					1, 0, 0,
-					0, 1, 0,
-					0, 0, 1;
-			}
+			// Same local order as in FEMBasis3d
+			simplex_corners_.resize(4, 3);
+			simplex_corners_ << 0, 0, 0,
+				1, 0, 0,
+				0, 1, 0,
+				0, 0, 1;
 		}
-		else
-		{
-			buf<<"Qqa"<<area_param_;
-			{
-				MatrixXd pts(4,2); pts <<
-				0,0,
-				0,1,
-				1,1,
-				1,0;
-
-				MatrixXi E(4,2); E <<
-				0,1,
-				1,2,
-				2,3,
-				3,0;
-
-				MatrixXd H(0,2);
-				igl::triangle::triangulate(pts, E, H, buf.str(), cube_points_, cube_faces_);
-
-				// Extract sampled edges matching the base element edges
-				igl::edges(cube_faces_, cube_edges_);
-				extract_parent_edges(cube_points_, cube_edges_, pts, E, cube_edges_);
-
-				// Same local order as in FEMBasis2d
-				cube_corners_.resize(4, 2);
-				cube_corners_ <<
-					0, 0,
-					1, 0,
-					1, 1,
-					0, 1;
-			}
-			{
-				triangulate_periodic(area_param_, simplex_points_, simplex_faces_, simplex_edges_);
-
-				// Same local order as in FEMBasis2d
-				simplex_corners_.resize(3, 2);
-				simplex_corners_ <<
-					0, 0,
-					1, 0,
-					0, 1;
-			}
-		}
-
 	}
-
-	void RefElementSampler::sample_polygon(const Eigen::MatrixXd &poly, Eigen::MatrixXd &pts, Eigen::MatrixXi &faces) const
+	else
 	{
-		MatrixXi E(poly.rows(),2);
-		for(int e = 0; e < int(poly.rows()); ++e)
 		{
-			E(e, 0) = e;
-			E(e, 1) = (e+1) % poly.rows();
+			MatrixXd pts(4, 2);
+			pts << 0, 0,
+				0, 1,
+				1, 1,
+				1, 0;
+
+			MatrixXi E(4, 2);
+			E << 0, 1,
+				1, 2,
+				2, 3,
+				3, 0;
+
+			regular_2d_grid(1. / sqrt(area_param_) + 1, false, cube_points_, cube_faces_);
+
+			// Extract sampled edges matching the base element edges
+			igl::edges(cube_faces_, cube_edges_);
+			extract_parent_edges(cube_points_, cube_edges_, pts, E, cube_edges_);
+
+			// Same local order as in FEMBasis2d
+			cube_corners_.resize(4, 2);
+			cube_corners_ << 0, 0,
+				1, 0,
+				1, 1,
+				0, 1;
 		}
-		std::stringstream buf;
-		buf.precision(100);
-		buf.setf(std::ios::fixed, std::ios::floatfield);
-		buf<<"Qqa"<<area_param_/1000.;
+		{
+			MatrixXd pts(3, 2);
+			pts << 0, 0,
+				0, 1,
+				1, 0;
 
-		igl::triangle::triangulate(poly, E, MatrixXd(0,2), buf.str(), pts, faces);
+			MatrixXi E(3, 2);
+			E << 0, 1,
+				1, 2,
+				2, 0;
+
+			regular_2d_grid(1. / sqrt(area_param_) + 1, true, simplex_points_, simplex_faces_);
+
+			// Extract sampled edges matching the base element edges
+			igl::edges(simplex_faces_, simplex_edges_);
+			extract_parent_edges(simplex_points_, simplex_edges_, pts, E, simplex_edges_);
+
+			// Same local order as in FEMBasis2d
+			simplex_corners_.resize(3, 2);
+			simplex_corners_ << 0, 0,
+				1, 0,
+				0, 1;
+		}
 	}
-
-	void RefElementSampler::sample_polyhedron(const Eigen::MatrixXd &vertices, const Eigen::MatrixXi &f, Eigen::MatrixXd &pts, Eigen::MatrixXi &faces) const
-	{
-		//TODO
-		pts = vertices;
-		faces = f;
-	}
-
 }
+
+void RefElementSampler::sample_polygon(const Eigen::MatrixXd &poly, Eigen::MatrixXd &pts, Eigen::MatrixXi &faces) const
+{
+	// MatrixXi E(poly.rows(), 2);
+	// for (int e = 0; e < int(poly.rows()); ++e)
+	// {
+	// 	E(e, 0) = e;
+	// 	E(e, 1) = (e + 1) % poly.rows();
+	// }
+	// std::stringstream buf;
+	// buf.precision(100);
+	// buf.setf(std::ios::fixed, std::ios::floatfield);
+	// buf << "Qqa" << area_param_ / 1000.;
+
+	// igl::triangle::triangulate(poly, E, MatrixXd(0, 2), buf.str(), pts, faces);
+	pts.resize(poly.rows()+1, poly.cols());
+	pts.block(0, 0, poly.rows(), poly.cols()) = poly;
+	pts.row(poly.rows()) = poly.colwise().mean();
+
+	faces.resize(poly.rows(), 3);
+	for (int e = 0; e < int(poly.rows()); ++e)
+	{
+		faces.row(e) << e , (e + 1) % poly.rows(), poly.rows();
+	}
+}
+
+void RefElementSampler::sample_polyhedron(const Eigen::MatrixXd &vertices, const Eigen::MatrixXi &f, Eigen::MatrixXd &pts, Eigen::MatrixXi &faces) const
+{
+	//TODO
+	pts = vertices;
+	faces = f;
+}
+
+} // namespace polyfem
