deb4c8b8942420ea3367b5b56ecce4ee5b24c77a
Huangzizhou <41658484+Huangzizhou@users.noreply.github.com>
2020-03-27 10:30:12 +0800

    compile on windows

---
+++ run.bat
@@ -0,0 +1,7 @@
+mkdir build
+
+cd build
+
+cmake .. -G "Visual Studio 16 2019" -DPARDISO_LIBRARIES="C:/Users/zizhou/Desktop/pardiso/libpardiso600-WIN-X86-64.lib" -DLAPACK_LIBRARIES="D:/LAPACK/lib/liblapack.dll.a" -DBLAS_LIBRARIES="D:/LAPACK/lib/libblas.dll.a"
+
+cmake --build .
