3f58d84cd0ad930b71e8c6db917fe46e8dd4e100
Teseo Schneider <teseo@bluewin.ch>
2020-07-07 19:01:11 -0400

    added binary vtu export

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -5,7 +5,7 @@
 #include <polyfem/ExpressionValue.hpp>
 #include <polyfem/MshReader.hpp>
 #include <polyfem/Mesh.hpp>
-
+#include <polyfem/VTUWriter.hpp>
 
 #include <Eigen/Dense>
 
@@ -97,4 +97,27 @@ TEST_CASE("mshreader", "[utils]") {
     Eigen::MatrixXi cells;
     const auto mesh = Mesh::create(path + "/circle2.msh");
     REQUIRE(mesh);
+}
+
+TEST_CASE("vtu_writer", "[utils]")
+{
+    Eigen::MatrixXd pts(25, 3);
+    pts << 0, 0, 0, 0.25, 0, 0, 0, 0.25, 0, 0.25, 0.25, 0,
+        0.5, 0, 0, 0.5, 0.25, 0, 0.75, 0, 0, 0.75, 0.25, 0,
+        1, 0, 0, 1, 0.25, 0, 0, 0.5, 0, 0.25, 0.5, 0,
+        0.5, 0.5, 0, 0.75, 0.5, 0, 1, 0.5, 0, 0, 0.75, 0,
+        0.25, 0.75, 0, 0.5, 0.75, 0, 0.75, 0.75, 0, 1, 0.75, 0,
+        0, 1, 0, 0.25, 1, 0, 0.5, 1, 0, 0.75, 1, 0,
+        1, 1, 0;
+    pts = pts.leftCols(2).eval();
+
+    Eigen::MatrixXd v(25, 1);
+    v.setRandom();
+
+    Eigen::MatrixXi tris(1, 3);
+    tris <<0, 1, 2;
+
+    VTUWriter writer;
+    writer.add_field("test", v);
+    writer.write_tet_mesh("test.vtu", pts, tris);
 }
