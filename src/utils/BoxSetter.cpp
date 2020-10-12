44e4bc5e93b4565a202a27e9bbd3464378239e37
Teseo Schneider <teseo@bluewin.ch>
2020-10-12 16:33:15 -0400

    added new selections

--- src/utils/BoxSetter.cpp
+++ src/utils/BoxSetter.cpp
@@ -1,15 +1,129 @@
 #include <polyfem/BoxSetter.hpp>
 
+#include <polyfem/Logger.hpp>
 #include <polyfem/Types.hpp>
+
+#include <memory>
 
 namespace polyfem
 {
-    typedef std::array<RowVectorNd, 2> BBox;
+    std::shared_ptr<Selection> Selection::build(const json &selection)
+    {
+        std::shared_ptr<Selection> res = nullptr;
+        if (selection.find("box") != selection.end())
+            res = std::make_shared<Box>(selection);
+        else if (selection.find("center") != selection.end())
+            res = std::make_shared<Sphere>(selection);
+        else if (selection.find("axis") != selection.end())
+            res = std::make_shared<AxisPlane>(selection);
+        else if (selection.find("normal") != selection.end())
+            res = std::make_shared<Plane>(selection);
+        else
+        {
+            logger().error("Selection not recognised: {}", selection.dump());
+        }
+
+        return res;
+    }
+
+    Box::Box(const json &selection)
+    {
+        auto bboxj = selection["box"];
+        const int dim = bboxj[0].size();
+        assert(bboxj[1].size() == dim);
+        bbox_[0].resize(dim);
+        bbox_[1].resize(dim);
+
+        for (size_t k = 0; k < dim; ++k)
+        {
+            bbox_[0][k] = bboxj[0][k];
+            bbox_[1][k] = bboxj[1][k];
+        }
+    }
+
+    bool Box::inside(const RowVectorNd &p) const
+    {
+        assert(bbox_[0].size() == p.size());
+        assert(bbox_[1].size() == p.size());
+        bool inside = true;
+
+        for (int d = 0; d < p.size(); ++d)
+        {
+            if (p[d] < bbox_[0][d] || p[d] > bbox_[1][d])
+            {
+                inside = false;
+                break;
+            }
+        }
+
+        return inside;
+    }
+
+    Sphere::Sphere(const json &selection)
+    {
+        auto center = selection["center"];
+        radius2_ = selection["radius"];
+        radius2_ *= radius2_;
+
+        const int dim = center.size();
+        center_.resize(dim);
+
+        for (size_t k = 0; k < dim; ++k)
+        {
+            center_[k] = center[k];
+        }
+    }
+
+    bool Sphere::inside(const RowVectorNd &p) const
+    {
+        assert(center_.size() == p.size());
+
+        return (p - center_).squaredNorm() <= radius2_;
+    }
+
+    AxisPlane::AxisPlane(const json &selection)
+    {
+        position_ = selection["position"];
+        axis_ = selection["axis"];
+    }
+
+    bool AxisPlane::inside(const RowVectorNd &p) const
+    {
+        const double v = p[std::abs(axis_) - 1];
+
+        if (axis_ > 0)
+            return v >= position_;
+        else
+            return v <= position_;
+    }
+
+    Plane::Plane(const json &selection)
+    {
+        auto normal = selection["normal"];
+        const int dim = normal.size();
+        normal_.resize(dim);
+
+        for (size_t k = 0; k < dim; ++k)
+        {
+            normal_[k] = normal[k];
+        }
+
+        const double offset = selection["offset"];
+        point_ = normal_ * offset;
+    }
+
+    bool Plane::inside(const RowVectorNd &p) const
+    {
+        assert(p.size() == normal_.size());
+        const RowVectorNd pp = p - point_;
+        return pp.dot(normal_) >= 0;
+    }
+
     namespace
     {
-        std::vector<std::pair<int, BBox>> get_boxes(const json &args, const std::string &key)
+        std::vector<std::pair<int, std::shared_ptr<Selection>>> get_selections(const json &args, const std::string &key)
         {
-            std::vector<std::pair<int, BBox>> boxes;
+            std::vector<std::pair<int, std::shared_ptr<Selection>>> selections;
 
             if (args.find(key) != args.end())
             {
@@ -18,51 +132,29 @@ namespace polyfem
 
                 for (size_t i = 0; i < boundary.size(); ++i)
                 {
-                    const auto box = boundary[i];
-                    int id = box["id"];
-                    auto bboxj = box["box"];
-                    BBox bbox;
-                    const int dim = bboxj[0].size();
-                    bbox[0].resize(dim);
-                    bbox[1].resize(dim);
+                    const auto selection = boundary[i];
+                    int id = selection["id"];
 
-                    for (size_t k = 0; k < dim; ++k)
-                    {
-                        bbox[0][k] = bboxj[0][k];
-                        bbox[1][k] = bboxj[1][k];
-                    }
-
-                    boxes.emplace_back(id, bbox);
+                    selections.emplace_back(id, Selection::build(selection));
                 }
             }
 
-            return boxes;
+            return selections;
         }
     } // namespace
 
     void BoxSetter::set_sidesets(const json &args, Mesh &mesh)
     {
-
-        std::vector<std::pair<int, BBox>> boundary = get_boxes(args, "boundary_sidesets");
-        std::vector<std::pair<int, BBox>> body = get_boxes(args, "body_ids");
+        std::vector<std::pair<int, std::shared_ptr<Selection>>> boundary = get_selections(args, "boundary_sidesets");
+        std::vector<std::pair<int, std::shared_ptr<Selection>>> body = get_selections(args, "body_ids");
 
         if (!boundary.empty())
         {
             mesh.compute_boundary_ids([&boundary](const RowVectorNd &p) {
                 for (const auto &b : boundary)
                 {
-                    const auto &bbox = b.second;
-                    assert(bbox[0].size() == p.size());
-                    bool inside = true;
-
-                    for (int d = 0; d < p.size(); ++d)
-                    {
-                        if (p[d] < bbox[0][d] || p[d] > bbox[1][d])
-                        {
-                            inside = false;
-                            break;
-                        }
-                    }
+                    const auto &selection = b.second;
+                    const bool inside = selection->inside(p);
 
                     if (inside)
                         return b.first;
@@ -77,18 +169,8 @@ namespace polyfem
             mesh.compute_body_ids([&body](const RowVectorNd &p) {
                 for (const auto &b : body)
                 {
-                    const auto &bbox = b.second;
-                    assert(bbox[0].size() == p.size());
-                    bool inside = true;
-
-                    for (int d = 0; d < p.size(); ++d)
-                    {
-                        if (p[d] < bbox[0][d] || p[d] > bbox[1][d])
-                        {
-                            inside = false;
-                            break;
-                        }
-                    }
+                    const auto &selection = b.second;
+                    const bool inside = selection->inside(p);
 
                     if (inside)
                         return b.first;
--- src/utils/BoxSetter.hpp
+++ src/utils/BoxSetter.hpp
@@ -5,6 +5,59 @@
 
 namespace polyfem
 {
+    class Selection
+    {
+    public:
+        virtual bool inside(const RowVectorNd &p) const = 0;
+        static std::shared_ptr<Selection> build(const json &selection);
+        virtual ~Selection() {}
+    };
+
+    class Box : public Selection
+    {
+    public:
+        typedef std::array<RowVectorNd, 2> BBox;
+
+        Box(const json &selection);
+        bool inside(const RowVectorNd &p) const override;
+
+    private:
+        BBox bbox_;
+    };
+
+    class Sphere : public Selection
+    {
+    public:
+        Sphere(const json &selection);
+        bool inside(const RowVectorNd &p) const override;
+
+    private:
+        RowVectorNd center_;
+        double radius2_;
+    };
+
+    class AxisPlane : public Selection
+    {
+    public:
+        AxisPlane(const json &selection);
+        bool inside(const RowVectorNd &p) const override;
+
+    private:
+        int axis_;
+        double position_;
+    };
+
+    class Plane : public Selection
+    {
+    public:
+        Plane(const json &selection);
+        bool inside(const RowVectorNd &p) const override;
+
+    private:
+        RowVectorNd normal_;
+        RowVectorNd point_;
+    };
+
     class BoxSetter
     {
     public:
