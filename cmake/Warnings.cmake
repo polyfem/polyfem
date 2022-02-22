cde60c778be387857661edf83d9e49410285038a
Teseo Schneider <teseo@bluewin.ch>
2022-02-22 10:40:57 -0800

    removed required cmake in warnings

--- cmake/Warnings.cmake
+++ cmake/Warnings.cmake
@@ -1,6 +1,4 @@
 ################################################################################
-cmake_minimum_required(VERSION 2.8.12)
-################################################################################
 # See comments and discussions here:
 # http://stackoverflow.com/questions/5088460/flags-to-enable-thorough-and-verbose-g-warnings
 ################################################################################
