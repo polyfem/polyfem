35d08f8f2a6ed4cc5057fe40aa495311ec545c78
Huangzizhou <zizhou@nyu.edu>
2022-03-22 22:40:29 -0400

    3d ncmesh test

--- tests/test_ncmesh.cpp
+++ tests/test_ncmesh.cpp
@@ -19,7 +19,7 @@
 
 using namespace polyfem;
 
-TEST_CASE("ncmesh", "[ncmesh]")
+TEST_CASE("ncmesh2d", "[ncmesh]")
 {
 	const std::string path = POLYFEM_DATA_DIR;
 	json in_args = R"(
@@ -76,4 +76,64 @@ TEST_CASE("ncmesh", "[ncmesh]")
 
 	REQUIRE(fabs(state.h1_semi_err) < 1e-9);
     REQUIRE(fabs(state.l2_err) < 1e-10);
+}
+
+
+TEST_CASE("ncmesh3d", "[ncmesh]")
+{
+	const std::string path = POLYFEM_DATA_DIR;
+	json in_args = R"(
+		{
+			"problem": "GenericScalar",
+			"scalar_formulation": "Laplacian",
+            "export":{
+                "high_order_mesh": false
+            },
+			"n_refs": 0,
+			"discr_order": 2,
+			"iso_parametric": false,
+            "bc_method": "sample",
+			"problem_params": {
+				"dirichlet_boundary": [{
+					"id": "all",
+					"value": "x^2+y^2+z^2"
+				}],
+                "exact": "x^2+y^2+z^2",
+                "exact_grad": ["2*x","2*y","2*z"],
+				"rhs": 6
+			},
+			"vismesh_rel_area": 1.0
+		}
+	)"_json;
+    std::string mesh_path = path + "/contact/meshes/3D/simple/torus.msh";
+
+	State state(8);
+	state.init_logger("", 6, false);
+	state.init(in_args);
+	
+    std::shared_ptr<ncMesh> ncmesh = std::make_shared<ncMesh3D>(mesh_path);
+
+    int id = 0;
+    int n = 0;
+    do {
+        id = rand() % ncmesh->elements.size();
+        if (ncmesh->elements[id].is_valid())
+        {
+            ncmesh->refineElement(id);
+            n++;
+        }
+    } while (n < 20);
+
+    state.load_ncMesh(ncmesh);
+	state.compute_mesh_stats();
+	state.build_basis();
+
+	state.assemble_stiffness_mat();
+	state.assemble_rhs();
+
+	state.solve_problem();
+    state.compute_errors();
+
+	REQUIRE(fabs(state.h1_semi_err) < 1e-8);
+    REQUIRE(fabs(state.l2_err) < 1e-9);
 }
