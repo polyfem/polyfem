cc5d1fb1b2dd669ece3ee0d7dd17550a6ac7b34a
Zachary Ferguson <zfergus@nyu.edu>
2022-05-16 06:38:24 -0400

    Updated wildmeshing toolkit

--- cmake/recipes/wmtk.cmake
+++ cmake/recipes/wmtk.cmake
@@ -12,7 +12,7 @@ include(FetchContent)
 FetchContent_Declare(
     wildmeshing_toolkit
     GIT_REPOSITORY https://github.com/wildmeshing/wildmeshing-toolkit.git
-    GIT_TAG c800e75ce283e43918702fb7c8875a9911f48e1e
+    GIT_TAG 762f1a1c3a1f553954d433af39f4db2d60ed52f9
     GIT_SHALLOW FALSE
 )
 FetchContent_MakeAvailable(wildmeshing_toolkit)
