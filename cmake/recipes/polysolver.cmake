71cdddf2d3c3cf9dde0c0ca4b3f7a6e839f3e5f8
Teseo Schneider <teseo@bluewin.ch>
2022-06-10 16:52:22 -0700

    updated polysolve

--- cmake/recipes/polysolver.cmake
+++ cmake/recipes/polysolver.cmake
@@ -12,7 +12,7 @@ include(FetchContent)
 FetchContent_Declare(
     polysolve
     GIT_REPOSITORY https://github.com/polyfem/polysolve.git
-    GIT_TAG b2c639c94b5550ee6e8e780b0e02cf0689a59eda
+    GIT_TAG 58dd11fe7c05adc88497861743bd42ce440b3e03
     GIT_SHALLOW FALSE
 )
 FetchContent_MakeAvailable(polysolve)
