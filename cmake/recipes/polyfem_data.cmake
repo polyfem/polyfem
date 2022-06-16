3567431de477fc23cf84c449f36ac7e5002b37b8
Zhongshi Jiang <jiangzs@nyu.edu>
2022-06-16 16:45:15 -0400

    comment CCD problem

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -7,7 +7,7 @@ include(FetchContent)
 FetchContent_Declare(
     polyfem_data
     GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-    GIT_TAG a2ff388f123fdd0e54345f4d473c699d7da3ea98
+    GIT_TAG 7f422a82c8c8fe84805c36f2833dff9cfeec08e5
     GIT_SHALLOW TRUE
     SOURCE_DIR ${POLYFEM_DATA_ROOT}
 )
