98d2fe5ac3500bd2749e11b73504de4918715a53
Teseo Schneider <teseo@bluewin.ch>
2022-06-17 09:25:32 -0400

    main compiles

--- src/polyfem/main.cpp
+++ src/polyfem/main.cpp
@@ -118,7 +118,9 @@ int main(int argc, char **argv)
 
 	CLI11_PARSE(command_line, argc, argv);
 
-
+	std::vector<std::string> names;
+	std::vector<Eigen::MatrixXi> cells;
+	std::vector<Eigen::MatrixXd> vertices;
 
 	json in_args = json({});
 
