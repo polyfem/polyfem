92c740e96278cf7d201f436c4f8994aabb4ae7f8
teseo@sonic <teseo@sonic>
2022-07-19 19:40:59 -0400

    updated data

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -7,7 +7,7 @@ include(FetchContent)
 FetchContent_Declare(
     polyfem_data
     GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-    GIT_TAG 7f422a82c8c8fe84805c36f2833dff9cfeec08e5
+    GIT_TAG c2a28d16ef489db53e6772220b4f1660eea8d684
     GIT_SHALLOW TRUE
     SOURCE_DIR ${POLYFEM_DATA_ROOT}
 )
