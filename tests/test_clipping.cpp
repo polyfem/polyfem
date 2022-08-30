ea16b9d1efe411f62130ccd0f64e39859b14d154
Zachary Ferguson <zfergus@nyu.edu>
2022-08-30 16:44:53 -0400

    Fixed clipping test

--- tests/test_clipping.cpp
+++ tests/test_clipping.cpp
@@ -2,11 +2,19 @@
 
 #include <polyfem/utils/ClipperUtils.hpp>
 
-#include <iostream>
+#include <ipc/utils/eigen_ext.hpp>
+
+namespace
+{
+	double tet_signed_volume(const Eigen::MatrixXd &T)
+	{
+		return ipc::cross(T.row(1) - T.row(0), T.row(2) - T.row(0)).dot(T.row(3) - T.row(0));
+	}
+} // namespace
 
 TEST_CASE("Tetrahedra clipping", "[clipping]")
 {
-	const double dz = 0.1; // GENERATE(take(100, random(-1.0, 1.0)));
+	const double dz = GENERATE(take(100, random(-1.0, 1.0)));
 
 	Eigen::MatrixXd subject_tet(4, 3);
 	subject_tet << 0, 0, 0,
@@ -20,19 +28,26 @@ TEST_CASE("Tetrahedra clipping", "[clipping]")
 	std::vector<Eigen::MatrixXd> r = polyfem::utils::TetrahedronClipping::clip(subject_tet, clipping_tet);
 
 	Eigen::MatrixXd expected_tet(4, 3);
-	expected_tet = (1 - dz) * subject_tet;
-	if (dz > 0)
-		expected_tet.col(2).array() += dz;
+	expected_tet = (1 - abs(dz)) * subject_tet;
+	expected_tet.col(2).array() += std::max(0.0, dz);
 
 	CAPTURE(dz);
 	REQUIRE(r.size() == 1);
-	std::cout << dz << std::endl;
-	std::cout << r[0] << std::endl;
-	std::cout << std::endl;
-	std::cout << subject_tet << std::endl;
-	std::cout << std::endl;
-	std::cout << clipping_tet << std::endl;
-	std::cout << std::endl;
-	std::cout << expected_tet << std::endl;
-	REQUIRE(r[0].isApprox(expected_tet));
+	REQUIRE(r[0].rows() == 4);
+	REQUIRE(r[0].cols() == 3);
+	CHECK(r[0].colwise().minCoeff().isApprox(expected_tet.colwise().minCoeff()));
+	CHECK(r[0].colwise().maxCoeff().isApprox(expected_tet.colwise().maxCoeff()));
+	CHECK(r[0].colwise().sum().isApprox(expected_tet.colwise().sum()));
+	CHECK(tet_signed_volume(r[0]) == Approx(tet_signed_volume(expected_tet)));
+	// Node order might be different
+	for (int i = 0; i < 4; i++)
+	{
+		int j = 0;
+		for (int j = 0; j < 4; j++)
+		{
+			if (r[0].row(i).isApprox(expected_tet.row(j)))
+				break;
+		}
+		CHECK(j < 4);
+	}
 }
