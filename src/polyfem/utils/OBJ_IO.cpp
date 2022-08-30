5d5f5ce410efdb9b3a1d75a2c0ff3cd61157241b
Huangzizhou <zizhou@nyu.edu>
2022-08-30 17:43:00 -0400

    fix bug in output intersection.obj

--- src/polyfem/utils/OBJ_IO.cpp
+++ src/polyfem/utils/OBJ_IO.cpp
@@ -363,7 +363,7 @@ namespace polyfem
 				return false;
 
 			obj << fmt::format(
-				"# Vertoces: {:d}\n# Edges: {:d}\n# Faces: {:d}",
+				"# Vertoces: {:d}\n# Edges: {:d}\n# Faces: {:d}\n",
 				v.rows(), e.rows(), f.rows());
 
 			for (int i = 0; i < v.rows(); ++i)
