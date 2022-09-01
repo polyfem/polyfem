d4f27f8cb463ae1f15d2c83f580e3961559450b6
Zachary Ferguson <zfergus@nyu.edu>
2022-09-01 08:59:16 -0400

    Updated MSHIO

--- cmake/recipes/mshio.cmake
+++ cmake/recipes/mshio.cmake
@@ -11,8 +11,8 @@ message(STATUS "Third-party: creating target 'mshio'")
 include(FetchContent)
 FetchContent_Declare(
     mshio
-    GIT_REPOSITORY https://github.com/qnzhou/MshIO.git
-    GIT_TAG a82fb59e25b5e4f586b29ec85a29c3bbc8117307
+    GIT_REPOSITORY https://github.com/zfergus/MshIO.git
+    GIT_TAG cc161de1447827aa401f799c4c3034d2ea1a2bea
     GIT_SHALLOW FALSE
 )
 FetchContent_MakeAvailable(mshio)
