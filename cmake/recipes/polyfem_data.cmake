f80e14372d36802d6bbc81f128fe926e5665d162
Zachary Ferguson <zfergus@nyu.edu>
2022-10-18 15:44:11 -0400

    Renamed POLYFEM_DATA_ROOT to POLYFEM_DATA_DIR

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -6,14 +6,13 @@ if(TARGET polyfem::data)
 endif()
 
 include(ExternalProject)
-include(FetchContent)
 
-set(POLYFEM_DATA_ROOT "${PROJECT_SOURCE_DIR}/data/" CACHE PATH "Where should polyfem download and look for test data?")
+set(POLYFEM_DATA_DIR "${PROJECT_SOURCE_DIR}/data/" CACHE PATH "Where should polyfem download and look for test data?")
 
 ExternalProject_Add(
     polyfem_data_download
     PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
-    SOURCE_DIR ${POLYFEM_DATA_ROOT}
+    SOURCE_DIR ${POLYFEM_DATA_DIR}
     GIT_REPOSITORY https://github.com/polyfem/polyfem-data
     GIT_TAG f9d6a461a4cdc341f4623db04a34c806427b2cb4
     CONFIGURE_COMMAND ""
@@ -28,5 +27,4 @@ add_library(polyfem::data ALIAS polyfem_data)
 
 add_dependencies(polyfem_data polyfem_data_download)
 
-target_compile_definitions(polyfem_data INTERFACE  POLYFEM_DATA_DIR=\"${POLYFEM_DATA_ROOT}\")
-SET(POLYFEM_DATA_DIR ${POLYFEM_DATA_ROOT})
+target_compile_definitions(polyfem_data INTERFACE  POLYFEM_DATA_DIR=\"${POLYFEM_DATA_DIR}\")
