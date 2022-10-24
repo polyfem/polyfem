ae6547bd3794b1638ab3cf9891d5acafee86f96c
teseo@sonic <teseo@sonic>
2022-10-24 13:58:51 -0400

    integration test uses eigen solver

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -27,7 +27,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-        GIT_TAG 5ce50074a3c57ce4d56948b825eba93df9f08c58
+        GIT_TAG f343e00cd3ee927e9a6442f935ed0c0d6f8b3510
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
