0c66756de9ede77608c688acbda464d8e0bdf9ff
Huangzizhou <zizhou@nyu.edu>
2022-10-28 16:43:08 -0400

    material opt unit test

--- tests/test_opt.cpp
+++ tests/test_opt.cpp
@@ -32,9 +32,12 @@ std::shared_ptr<State> create_state(const json &args)
 	std::shared_ptr<State> state = std::make_shared<State>();
 	state->init_logger("", spdlog::level::level_enum::err, false);
 	state->init(args, false);
+	state->args["optimization"]["enabled"] = true;
 	state->load_mesh();
 	Eigen::MatrixXd sol, pressure;
 	state->build_basis();
+	state->assemble_rhs();
+	state->assemble_stiffness_mat();
 
 	return state;
 }
@@ -49,24 +52,82 @@ void solve_pde(State &state)
 
 } // namespace
 
-TEST_CASE("shape-trajectory-surface-opt", "[optimization]")
+void run_trajectory_opt(const std::string &name)
 {
-	const std::string path = POLYFEM_DATA_DIR + std::string("/../optimizations/shape-trajectory-surface-opt");
+	const std::string path = POLYFEM_DATA_DIR + std::string("/../optimizations/" + name);
+	
 	json target_args, in_args;
-    load_json(path + "/target.json", target_args);
 	load_json(path + "/run.json", in_args);
-
+    load_json(path + "/target.json", target_args);
+	auto state = create_state(in_args);
 	auto target_state = create_state(target_args);
 	solve_pde(*target_state);
 
-	auto state = create_state(in_args);
+	auto opt_params = state->args["optimization"];
+	auto objective_params = opt_params["functionals"][0];
 
-	std::shared_ptr<CompositeFunctional> func = CompositeFunctional::create("Trajectory");
-	auto &f = *dynamic_cast<TrajectoryFunctional *>(func.get());
-	f.set_interested_ids({2}, {});
-	f.set_reference(target_state.get(), *state, {2});
+	std::string matching_type = objective_params["matching"];
+	std::shared_ptr<CompositeFunctional> func;
+	if (objective_params["type"] == "trajectory")
+	{
+		if (matching_type == "exact")
+			func = CompositeFunctional::create("Trajectory");
+		else if (matching_type == "sdf")
+			func = CompositeFunctional::create("SDFTrajectory");
+		else if (matching_type == "exact-center")
+			func = CompositeFunctional::create("CenterTrajectory");
+		else if (matching_type == "last-center")
+			func = CompositeFunctional::create("CenterXZTrajectory");
+		else if (matching_type == "sine")
+			func = CompositeFunctional::create("TargetY");
+		else if (matching_type == "center-data")
+			func = CompositeFunctional::create("CenterXYTrajectory");
+		else if (matching_type == "marker-data")
+			func = CompositeFunctional::create("NodeTrajectory");
+		else
+			logger().error("Invalid matching type!");
+	}
+	else if (objective_params["type"] == "height")
+	{
+		func = CompositeFunctional::create("Height");
+	}
+
+	std::string transient_integral_type = objective_params["transient_integral_type"];
+	if (transient_integral_type != "")
+		func->set_transient_integral_type(transient_integral_type);
+
+	std::set<int> interested_body_ids;
+	std::vector<int> interested_bodies = objective_params["volume_selection"];
+	interested_body_ids = std::set(interested_bodies.begin(), interested_bodies.end());
+
+	std::set<int> interested_boundary_ids;
+	std::vector<int> interested_boundaries = objective_params["surface_selection"];
+	interested_boundary_ids = std::set(interested_boundaries.begin(), interested_boundaries.end());
+
+	func->set_interested_ids(interested_body_ids, interested_boundary_ids);
+
+	if (matching_type == "exact")
+	{
+		auto &f = *dynamic_cast<TrajectoryFunctional *>(func.get());
+
+		std::set<int> reference_cached_body_ids;
+		if (objective_params["reference_cached_body_ids"].size() > 0)
+		{
+			std::vector<int> ref_cached = objective_params["reference_cached_body_ids"];
+			reference_cached_body_ids = std::set(ref_cached.begin(), ref_cached.end());
+		}
+		else
+			reference_cached_body_ids = interested_body_ids;
+
+		f.set_reference(target_state.get(), *state, reference_cached_body_ids);
+	}
 
 	CHECK_THROWS_WITH(general_optimization(*state, func), Catch::Matchers::Contains("Reached iteration limit"));
+}
+
+TEST_CASE("shape-trajectory-surface-opt", "[optimization]")
+{
+	run_trajectory_opt("shape-trajectory-surface-opt");
 
 	std::ifstream energy_out("shape-trajectory-surface-opt");
 	std::vector<double> energies;
@@ -126,4 +187,28 @@ TEST_CASE("shape-stress-opt", "[optimization]")
 
 	REQUIRE(starting_energy  == Approx(12.0721).epsilon(1e-4));
 	REQUIRE(optimized_energy == Approx(11.3404).epsilon(1e-4));
+}
+
+TEST_CASE("material-opt", "[optimization]")
+{
+	run_trajectory_opt("material-opt");
+
+	std::ifstream energy_out("material-opt");
+	std::vector<double> energies;
+	std::string line;
+	if (energy_out.is_open())
+	{
+		while (getline(energy_out, line))
+		{
+			energies.push_back(std::stod(line.substr(0, line.find(","))));
+		}
+	}
+	double starting_energy = energies[0];
+	double optimized_energy = energies[energies.size() - 1];
+
+	std::cout << starting_energy << std::endl;
+	std::cout << optimized_energy << std::endl;
+
+	REQUIRE(starting_energy  == Approx(0.00143472).epsilon(1e-4));
+	REQUIRE(optimized_energy == Approx(1.10657e-05).epsilon(1e-4));
 }
