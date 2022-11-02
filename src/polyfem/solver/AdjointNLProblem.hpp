de97677260870dfc60295e238d1d4b2af1b51eb0
Arvi Gjoka <ag4571@nyu.edu>
2022-11-02 11:42:19 -0400

    Refactor optimization

--- src/polyfem/solver/AdjointNLProblem.hpp
+++ src/polyfem/solver/AdjointNLProblem.hpp
@@ -2,50 +2,56 @@
 
 #include <cppoptlib/problem.h>
 #include "Objective.hpp"
+#include "Parameter.hpp"
 
 namespace polyfem::solver
 {
 	class AdjointNLProblem : public cppoptlib::Problem<double>
 	{
 	public:
-		using typename cppoptlib::Problem<double>::Scalar;
-		using typename cppoptlib::Problem<double>::TVector;
+		AdjointNLProblem(std::vector<std::shared_ptr<Objective>> &objectives, std::vector<std::shared_ptr<Parameter>> parameters, std::vector<std::shared_ptr<State>> all_states) : objectives_(objectives), parameters_(parameters), all_states_(all_states)
+		{
+			for (const auto p : parameters)
+				optimization_dim_ += p->optimization_dim();
+		}
 
-		AdjointNLProblem(std::vector<std::shared_ptr<Objective>> &objectives): objectives_(objectives)
-		{}
+		double target_value(const Eigen::VectorXd &x);
+		double value(const Eigen::VectorXd &x);
+		double value(const Eigen::VectorXd &x, const bool only_elastic);
 
-		double target_value(const TVector &x);
-		double value(const TVector &x);
-		double value(const TVector &x, const bool only_elastic);
+		void target_gradient(const Eigen::VectorXd &x, Eigen::VectorXd &gradv);
+		void gradient(const Eigen::VectorXd &x, Eigen::VectorXd &gradv);
+		void gradient(const Eigen::VectorXd &x, Eigen::VectorXd &gradv, const bool only_elastic);
 
-		void target_gradient(const TVector &x, TVector &gradv);
-		void gradient(const TVector &x, TVector &gradv);
-		void gradient(const TVector &x, TVector &gradv, const bool only_elastic);
+		void smoothing(const Eigen::VectorXd &x, Eigen::VectorXd &new_x);
+		bool remesh(Eigen::VectorXd &x);
 
-		void smoothing(const TVector &x, TVector &new_x);
-		bool remesh(TVector &x);
+		bool is_step_valid(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) const;
+		bool is_intersection_free(const Eigen::VectorXd &x) const;
+		bool is_step_collision_free(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) const;
+		double max_step_size(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) const;
 
-		bool is_step_valid(const TVector &x0, const TVector &x1) const;
-		bool is_intersection_free(const TVector &x) const;
-		bool is_step_collision_free(const TVector &x0, const TVector &x1) const;
-		double max_step_size(const TVector &x0, const TVector &x1) const;
-
-		void line_search_begin(const TVector &x0, const TVector &x1);
+		void line_search_begin(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1);
 		void line_search_end(bool failed);
-		void post_step(const int iter_num, const TVector &x);
-		void save_to_file(const TVector &x0);
+		void post_step(const int iter_num, const Eigen::VectorXd &x);
+		void save_to_file(const Eigen::VectorXd &x0);
 
-		void solution_changed(const TVector &new_x);
+		void solution_changed(const Eigen::VectorXd &new_x);
 
-		TVector get_lower_bound(const TVector &x) const;
-		TVector get_upper_bound(const TVector &x) const;
+		Eigen::VectorXd get_lower_bound(const Eigen::VectorXd &x) const;
+		Eigen::VectorXd get_upper_bound(const Eigen::VectorXd &x) const;
 
 		int n_inequality_constraints();
-		TVector force_inequality_constraint(const TVector &x0, const TVector &dx);
-		double inequality_constraint_val(const TVector &x, const int index);
-		TVector inequality_constraint_grad(const TVector &x, const int index);
+		Eigen::VectorXd force_inequality_constraint(const Eigen::VectorXd &x0, const Eigen::VectorXd &dx);
+		double inequality_constraint_val(const Eigen::VectorXd &x, const int index);
+		Eigen::VectorXd inequality_constraint_grad(const Eigen::VectorXd &x, const int index);
 
 	private:
+		int optimization_dim_;
 		std::vector<std::shared_ptr<Objective>> objectives_;
+		std::vector<std::shared_ptr<Parameter>> parameters_;
+		std::vector<std::shared_ptr<State>> all_states_;
+		Eigen::VectorXd cur_x;
+		int iter = 0;
 	};
 } // namespace polyfem::solver
--- src/polyfem/solver/Parameter.hpp
+++ src/polyfem/solver/Parameter.hpp
@@ -1,17 +1,68 @@
 #pragma once
 
-#include "OptimizationProblem.hpp"
+#include <polyfem/State.hpp>
 
 namespace polyfem
 {
 	class Parameter
 	{
 	public:
-		Parameter(std::vector<State> states_ptr) : states_ptr_(states_ptr){};
+		Parameter(std::vector<std::shared_ptr<State>> states_ptr) : states_ptr_(states_ptr){};
+		virtual ~Parameter() = default;
 
 		virtual void update() = 0;
 
+		virtual void map(const Eigen::MatrixXd &x, Eigen::MatrixXd &q) = 0;
+
+		virtual void smoothing(const Eigen::VectorXd &x, Eigen::VectorXd &new_x) = 0;
+		virtual bool is_step_valid(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) = 0;
+		virtual bool is_intersection_free(const Eigen::VectorXd &x) = 0;
+		virtual bool is_step_collision_free(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) { return true; }
+		virtual double max_step_size(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) { return 1; }
+
+		virtual void line_search_begin(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) = 0;
+		virtual void line_search_end(bool failed) = 0;
+		virtual void post_step(const int iter_num, const Eigen::VectorXd &x0) = 0;
+
+		inline virtual void set_optimization_dim(const int optimization_dim) final { optimization_dim_ = optimization_dim; }
+		inline virtual int optimization_dim() final { return optimization_dim_; }
+
+		inline virtual std::string name() final { return parameter_name_; }
+
+		virtual bool pre_solve(const Eigen::VectorXd &newX) = 0;
+		virtual void post_solve(const Eigen::VectorXd &newX) = 0;
+
+		inline virtual bool remesh(Eigen::VectorXd &x) { return true; };
+
+		virtual Eigen::VectorXd force_inequality_constraint(const Eigen::VectorXd &x0, const Eigen::VectorXd &dx) { return x0 + dx; }
+		virtual int n_inequality_constraints() { return 0; }
+		virtual double inequality_constraint_val(const Eigen::VectorXd &x, const int index)
+		{
+			assert(false);
+			return std::nan("");
+		}
+		virtual Eigen::VectorXd inequality_constraint_grad(const Eigen::VectorXd &x, const int index)
+		{
+			assert(false);
+			return Eigen::VectorXd();
+		}
+
+		virtual Eigen::VectorXd get_lower_bound(const Eigen::VectorXd &x) const
+		{
+			Eigen::VectorXd min(x.size());
+			min.setConstant(std::numeric_limits<double>::min());
+			return min;
+		}
+		virtual Eigen::VectorXd get_upper_bound(const Eigen::VectorXd &x) const
+		{
+			Eigen::VectorXd max(x.size());
+			max.setConstant(std::numeric_limits<double>::max());
+			return max;
+		}
+
 	private:
 		std::vector<std::shared_ptr<State>> states_ptr_;
+		int optimization_dim_;
+		const std::string parameter_name_;
 	};
 } // namespace polyfem
