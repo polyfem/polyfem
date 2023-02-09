c88fa011bd89a3c452c95820734a7de016153689
Arvi Gjoka <ag4571@nyu.edu>
2023-02-09 11:56:08 -0500

    update variables to simulatioon

--- src/polyfem/solver/forms/adjoint_forms/VariablesToSimulation.hpp
+++ src/polyfem/solver/forms/adjoint_forms/VariablesToSimulation.hpp
@@ -4,25 +4,40 @@
 
 namespace polyfem::solver
 {
+	namespace
+	{
+
+		void update_shape(std::shared_ptr<State> state_ptr)
+		{
+		}
+
+		void update_material_params(std::shared_ptr<State> state_ptr)
+		{
+		}
+
+	} // namespace
 	class VariablesToSimulation
 	{
-		VariablesToSimulation(std::shared_ptr<State> state_ptr, const CompositeParametrization &parametrizations, const ParameterTypes &parameter_types) : state_ptr_(state_ptr), parametrizations_(parametrizations), parameter_types_(parameter_types)
+		VariablesToSimulation(std::shared_ptr<State> state_ptr, const CompositeParametrization &parametrization, const ParameterTypes &parameter_type) : state_ptr_(state_ptr), parametrization_(parametrization), parameter_type_(parameter_type)
 		{
 		}
 
 		inline void update(const Eigen::VectorXd &x)
 		{
+			switch (parameter_type)
+			{
+			}
 		}
 
 		inline Eigen::VectorXd apply_jacobian(const Eigen::VectorXd &grad_full, const Eigen::VectorXd &x) const
 		{
-			return Eigen::VectorXd();
+			return parametrization_->apply_jacobian(grad_full, x);
 		}
 
 	private:
 		std::shared_ptr<State> state_ptr_;
-		CompositeParametrization parametrizations_;
-		ParameterType parameter_types_;
+		CompositeParametrization parametrization_;
+		ParameterType parameter_type_;
 	}
 
 } // namespace polyfem::solver
