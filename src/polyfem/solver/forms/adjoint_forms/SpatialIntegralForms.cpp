7cb89450acdf480ef3ae74e6e3fd8a82654a9198
Huangzizhou <zizhou@nyu.edu>
2023-02-09 15:30:47 -0500

    stress form

---
+++ src/polyfem/solver/forms/adjoint_forms/SpatialIntegralForms.cpp
@@ -0,0 +1,142 @@
+#include "SpatialIntegralForms.hpp"
+
+namespace polyfem::solver
+{
+	double SpatialIntegralForm::value_unweighted(const Eigen::VectorXd &x) const
+	{
+		assert(time_step_ < state_.diff_cached.size());
+		return AdjointTools::integrate_objective(state_, get_integral_functional(), state_.diff_cached[time_step_].u, ids_, spatial_integral_type_, time_step_);
+	}
+
+	void SpatialIntegralForm::compute_partial_gradient(const Eigen::VectorXd &x, Eigen::VectorXd &gradv)
+	{
+		assert(time_step_ < state_.diff_cached.size());
+		gradv.setZero(x.size());
+		for (const auto &param_map : *variable_to_simulations)
+		{
+			const auto &parametrization = param_map.get_parameterization();
+			const auto &state = param_map.get_state();
+			const auto &param_type = param_map.get_parameter_type();
+
+			if (&state != &state_)
+				continue;
+
+			Eigen::VectorXd term;
+			if (param_type == ParameterType::Shape)
+				AdjointTools::compute_shape_derivative_functional_term(state_, state_.diff_cached[time_step_].u, get_integral_functional(), ids_, spatial_integral_type_, term, time_step_);
+			else if (param_type == ParameterType::MacroStrain)
+				AdjointTools::compute_macro_strain_derivative_functional_term(state_, state_.diff_cached[time_step_].u, get_integral_functional(), ids_, spatial_integral_type_, term, time_step_);
+      
+      if (term.size() > 0)
+        gradv += parametrization.apply_jacobian(term);
+		}
+	}
+
+	Eigen::MatrixXd SpatialIntegralForm::compute_adjoint_rhs(const Eigen::VectorXd &x, const State &state)
+	{
+		if (&state != &state_)
+			return Eigen::VectorXd::Zero(state.ndof(), state.diff_cached.size());
+
+		assert(time_step_ < state_.diff_cached.size());
+
+		Eigen::VectorXd rhs;
+		AdjointTools::dJ_du_step(*state, get_integral_functional(), state.diff_cached[time_step_].u, interested_ids_, spatial_integral_type_, time_step_, rhs);
+
+		return rhs;
+	}
+
+	// TODO: call local assemblers instead
+	IntegrableFunctional StressForm::get_integral_functional()
+	{
+		IntegrableFunctional j;
+
+		const std::string formulation = state_.formulation();
+		const int power = in_power_;
+
+		j.set_j([formulation, power](const Eigen::MatrixXd &local_pts, const Eigen::MatrixXd &pts, const Eigen::MatrixXd &u, const Eigen::MatrixXd &grad_u, const Eigen::MatrixXd &lambda, const Eigen::MatrixXd &mu, const json &params, Eigen::MatrixXd &val) {
+			val.setZero(grad_u.rows(), 1);
+			Eigen::MatrixXd grad_u_q, stress;
+			for (int q = 0; q < grad_u.rows(); q++)
+			{
+				if (formulation == "Laplacian")
+				{
+					stress = grad_u.row(q);
+				}
+				else if (formulation == "LinearElasticity")
+				{
+					vector2matrix(grad_u.row(q), grad_u_q);
+					stress = mu(q) * (grad_u_q + grad_u_q.transpose()) + lambda(q) * grad_u_q.trace() * Eigen::MatrixXd::Identity(grad_u_q.rows(), grad_u_q.cols());
+				}
+				else if (formulation == "NeoHookean")
+				{
+					vector2matrix(grad_u.row(q), grad_u_q);
+					Eigen::MatrixXd def_grad = Eigen::MatrixXd::Identity(grad_u_q.rows(), grad_u_q.cols()) + grad_u_q;
+					Eigen::MatrixXd FmT = def_grad.inverse().transpose();
+					stress = mu(q) * (def_grad - FmT) + lambda(q) * std::log(def_grad.determinant()) * FmT;
+				}
+				else
+					log_and_throw_error("Unknown formulation!");
+				val(q) = pow(stress.squaredNorm(), power / 2.);
+			}
+		});
+
+		j.set_dj_dgradu([formulation, power](const Eigen::MatrixXd &local_pts, const Eigen::MatrixXd &pts, const Eigen::MatrixXd &u, const Eigen::MatrixXd &grad_u, const Eigen::MatrixXd &lambda, const Eigen::MatrixXd &mu, const json &params, Eigen::MatrixXd &val) {
+			val.setZero(grad_u.rows(), grad_u.cols());
+			const int dim = sqrt(grad_u.cols());
+			const int actual_dim = (formulation == "Laplacian") ? 1 : dim;
+			Eigen::MatrixXd grad_u_q, stress, stress_dstress;
+			for (int q = 0; q < grad_u.rows(); q++)
+			{
+				if (formulation == "Laplacian")
+				{
+					stress = grad_u.row(q);
+					stress_dstress = 2 * stress;
+				}
+				else if (formulation == "LinearElasticity")
+				{
+					vector2matrix(grad_u.row(q), grad_u_q);
+					stress = mu(q) * (grad_u_q + grad_u_q.transpose()) + lambda(q) * grad_u_q.trace() * Eigen::MatrixXd::Identity(grad_u_q.rows(), grad_u_q.cols());
+					stress_dstress = mu(q) * (stress + stress.transpose()) + lambda(q) * stress.trace() * Eigen::MatrixXd::Identity(stress.rows(), stress.cols());
+				}
+				else if (formulation == "NeoHookean")
+				{
+					vector2matrix(grad_u.row(q), grad_u_q);
+					Eigen::MatrixXd def_grad = Eigen::MatrixXd::Identity(grad_u_q.rows(), grad_u_q.cols()) + grad_u_q;
+					Eigen::MatrixXd FmT = def_grad.inverse().transpose();
+					stress = mu(q) * (def_grad - FmT) + lambda(q) * std::log(def_grad.determinant()) * FmT;
+					stress_dstress = mu(q) * stress + FmT * stress.transpose() * FmT * (mu(q) - lambda(q) * std::log(def_grad.determinant())) + (lambda(q) * (FmT.array() * stress.array()).sum()) * FmT;
+				}
+				else
+					log_and_throw_error("Unknown formulation!");
+
+				const double coef = power * pow(stress.squaredNorm(), power / 2. - 1.);
+				for (int i = 0; i < actual_dim; i++)
+					for (int l = 0; l < dim; l++)
+						val(q, i * dim + l) = coef * stress_dstress(i, l);
+			}
+		});
+
+		return j;
+	}
+
+  void StressForm::compute_partial_gradient(const Eigen::VectorXd &x, Eigen::VectorXd &gradv)
+  {
+    SpatialIntegralForm::compute_partial_gradient(x, gradv);
+		for (const auto &param_map : *variable_to_simulations)
+		{
+			const auto &parametrization = param_map.get_parameterization();
+			const auto &state = param_map.get_state();
+			const auto &param_type = param_map.get_parameter_type();
+
+			if (&state != &state_)
+				continue;
+
+			Eigen::VectorXd term;
+			if (param_type == ParameterType::Material)
+				log_and_throw_error("Doesn't support stress derivative wrt. material!");
+
+      if (term.size() > 0)
+        gradv += parametrization.apply_jacobian(term);
+		}
+  }
+} // namespace polyfem::solver
--- src/polyfem/solver/forms/adjoint_forms/SpatialIntegralForms.hpp
+++ src/polyfem/solver/forms/adjoint_forms/SpatialIntegralForms.hpp
@@ -7,6 +7,40 @@ namespace polyfem::solver
     class SpatialIntegralForm : public AdjointForm
     {
     public:
-        SpatialIntegralObjective(const State &state, const json &args);
+        SpatialIntegralForm(const State &state, const json &args): state_(state) {}
+
+        const State &get_state() { return state_; }
+        
+    protected:
+
+        virtual IntegrableFunctional get_integral_functional() = 0;
+        double value_unweighted(const Eigen::VectorXd &x) const override;
+        virtual void compute_partial_gradient(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) override;
+        Eigen::MatrixXd compute_adjoint_rhs(const Eigen::VectorXd &x, const State &state) override;
+        
+        const State &state_;
+        SpatialIntegralType spatial_integral_type_;
+        std::set<int> ids_;
+        
+        int time_step_ = 0;
+    };
+
+    class StressForm : public SpatialIntegralForm
+    {
+    public:
+        StressForm(const State &state, const json &args): SpatialIntegralForm(state, args) 
+        {
+            spatial_integral_type_ = SpatialIntegralType::VOLUME;
+
+            auto tmp_ids = args["volume_selection"].get<std::vector<int>>();
+            ids_ = std::set(tmp_ids.begin(), tmp_ids.end());
+        }
+
+    protected:
+        IntegrableFunctional get_integral_functional() override;
+        void compute_partial_gradient(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) override;
+
+    private:
+        int in_power_ = 2;
     };
 }
