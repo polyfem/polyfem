007466ec708aa6e03783df8ed07bdaaf07cad611
Huangzizhou <zizhou@nyu.edu>
2023-02-08 21:20:47 -0500

    single composite parametrization to avoid exposing for loops

--- src/polyfem/solver/forms/parameterization/Parameterization.hpp
+++ src/polyfem/solver/forms/parameterization/Parameterization.hpp
@@ -24,4 +24,49 @@ namespace polyfem::solver
 		virtual Eigen::VectorXd eval(const Eigen::VectorXd &x) const = 0;
 		virtual Eigen::VectorXd chain_rule(const Eigen::VectorXd &grad_full, const Eigen::VectorXd &x) const = 0;
 	};
+
+	class CompositeParameterization
+	{
+	public:
+		virtual ~CompositeParameterization() {}
+
+		Eigen::VectorXd inverse_eval(const Eigen::VectorXd &y) const
+		{
+			log_and_throw_error("Not supported");
+			return Eigen::VectorXd();
+		}
+
+		Eigen::VectorXd eval(const Eigen::VectorXd &x) const
+		{
+			if (parameterizations_.empty())
+				return x;
+
+			Eigen::VectorXd y = x;
+			for (const auto &p : parameterizations_)
+			{
+				y = p->eval(y);
+			}
+
+			return y;
+		}
+		Eigen::VectorXd chain_rule(const Eigen::VectorXd &x, const Eigen::VectorXd &grad_full) const
+		{
+			Eigen::VectorXd gradv = grad_full;
+
+			std::vector<Eigen::VectorXd> ys;
+			for (const auto &p : parameterizations_)
+			{
+				ys.emplace_back(y);
+				y = p->eval(y);
+			}
+
+			for (int i = parameterizations_.size() - 1; i >= 0; --i)
+			{
+				gradv = parameterizations_[i]->chain_rule(gradv, ys[i]);
+			}
+		}
+	
+	private:
+		std::vector<std::shared_ptr<Parameterization>> parameterizations_;
+	};
 } // namespace polyfem::solver
