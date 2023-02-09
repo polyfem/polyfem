38f2f3d6ff09d8f7668d085b079da1c84daedb9c
Huangzizhou <zizhou@nyu.edu>
2023-02-08 21:20:59 -0500

    gradient functions in adjoint form

---
+++ src/polyfem/solver/forms/adjoint_forms/AdjointForm.cpp
@@ -0,0 +1,33 @@
+#include "AdjointForm.hpp"
+
+namespace polyfem::solver
+{
+    void AdjointForm::first_derivative_unweighted(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) const
+    {
+        gradv.setZero(x.size());
+        for (const auto &tuple : variable_to_simulation)
+        {
+            const auto &parametrization = std::get<0>(tuple);
+            const auto &state = std::get<1>(tuple);
+            const auto &param_type = std::get<2>(tuple);
+
+            gradv += parametrization.chain_rule(x, compute_adjoint_term(*state, param_type));
+        }
+
+        Eigen::VectorXd partial_grad;
+        compute_partial_gradient(x, partial_grad);
+        gradv += partial_grad;
+    }
+
+    Eigen::VectorXd AdjointForm::compute_partial_gradient(const Eigen::VectorXd &x, Eigen::VectorXd &gradv)
+    {
+        log_and_throw_error("Should override this function in any AdjointForm!");
+    }
+
+    Eigen::VectorXd AdjointForm::compute_adjoint_term(const State &state, const ParameterType &param) const
+    {
+        Eigen::VectorXd term;
+        AdjointTools::compute_adjoint_term(state, state.get_adjoint_mat(), param, term);
+        return term;
+    }
+}
--- src/polyfem/solver/forms/adjoint_forms/AdjointForm.hpp
+++ src/polyfem/solver/forms/adjoint_forms/AdjointForm.hpp
@@ -1,22 +1,20 @@
-#include "../ParametrizationForm.hpp"
+#pragma once
+
+#include <polyfem/solver/forms/ParametrizationForm.hpp>
+#include <polyfem/solver/AdjointTools.hpp>
 
 namespace polyfem::solver
 {
-    enum class ParameterType {
-        Shape, Material, InitialCondition, DirichletBoundary
-    };
-
     class AdjointForm : public ParametrizationForm 
     {
     public:
         virtual ~AdjointForm() {}
 
     protected:
-        virtual void first_derivative_unweighted(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) const;
+        virtual void first_derivative_unweighted(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) const final override;
+        virtual Eigen::VectorXd compute_adjoint_term(const State &state, const ParameterType &param) const final;
+        virtual Eigen::VectorXd compute_partial_gradient(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) override;
 
-        virtual Eigen::VectorXd compute_partial_gradient(const Parameter &param, const Eigen::VectorXd &param_value);
-        static Eigen::VectorXd compute_adjoint_term(const State &state, const Eigen::MatrixXd &adjoints, const Parameter &param);
-
-        std::vector<std::pair<std::shared_ptr<State>, std::vector<Parameter>>> states;
+        std::vector<std::tuple<CompositeParameterization, std::shared_ptr<State>, std::vector<ParameterType>>> variable_to_simulation; // parameterizations that map from opt variables to simulations
     };
 }
