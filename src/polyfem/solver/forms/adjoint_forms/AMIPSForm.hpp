cd004eb00c8870860ea548c7d182692c4efe9b5e
Arvi Gjoka <ag4571@nyu.edu>
2023-02-14 10:52:18 -0500

    add step valid to AMIPSForm

--- src/polyfem/solver/forms/adjoint_forms/AMIPSForm.hpp
+++ src/polyfem/solver/forms/adjoint_forms/AMIPSForm.hpp
@@ -11,6 +11,84 @@
 
 namespace polyfem::solver
 {
+	namespace
+	{
+		double triangle_jacobian(const Eigen::VectorXd &v1, const Eigen::VectorXd &v2, const Eigen::VectorXd &v3)
+		{
+			Eigen::VectorXd a = v2 - v1, b = v3 - v1;
+			return a(0) * b(1) - b(0) * a(1);
+		}
+
+		double tet_determinant(const Eigen::VectorXd &v1, const Eigen::VectorXd &v2, const Eigen::VectorXd &v3, const Eigen::VectorXd &v4)
+		{
+			Eigen::Matrix3d mat;
+			mat.col(0) << v2 - v1;
+			mat.col(1) << v3 - v1;
+			mat.col(2) << v4 - v1;
+			return mat.determinant();
+		}
+
+		void scaled_jacobian(const Eigen::MatrixXd &V, const Eigen::MatrixXi &F, Eigen::VectorXd &quality)
+		{
+			const int dim = F.cols() - 1;
+
+			quality.setZero(F.rows());
+			if (dim == 2)
+			{
+				for (int i = 0; i < F.rows(); i++)
+				{
+					Eigen::RowVector3d e0;
+					e0(2) = 0;
+					e0.head(2) = V.row(F(i, 2)) - V.row(F(i, 1));
+					Eigen::RowVector3d e1;
+					e1(2) = 0;
+					e1.head(2) = V.row(F(i, 0)) - V.row(F(i, 2));
+					Eigen::RowVector3d e2;
+					e2(2) = 0;
+					e2.head(2) = V.row(F(i, 1)) - V.row(F(i, 0));
+
+					double l0 = e0.norm();
+					double l1 = e1.norm();
+					double l2 = e2.norm();
+
+					double A = 0.5 * (e0.cross(e1)).norm();
+					double Lmax = std::max(l0 * l1, std::max(l1 * l2, l0 * l2));
+
+					quality(i) = 2 * A * (2 / sqrt(3)) / Lmax;
+				}
+			}
+			else
+			{
+				for (int i = 0; i < F.rows(); i++)
+				{
+					Eigen::RowVector3d e0 = V.row(F(i, 1)) - V.row(F(i, 0));
+					Eigen::RowVector3d e1 = V.row(F(i, 2)) - V.row(F(i, 1));
+					Eigen::RowVector3d e2 = V.row(F(i, 0)) - V.row(F(i, 2));
+					Eigen::RowVector3d e3 = V.row(F(i, 3)) - V.row(F(i, 0));
+					Eigen::RowVector3d e4 = V.row(F(i, 3)) - V.row(F(i, 1));
+					Eigen::RowVector3d e5 = V.row(F(i, 3)) - V.row(F(i, 2));
+
+					double l0 = e0.norm();
+					double l1 = e1.norm();
+					double l2 = e2.norm();
+					double l3 = e3.norm();
+					double l4 = e4.norm();
+					double l5 = e5.norm();
+
+					double J = std::abs((e0.cross(e3)).dot(e2));
+
+					double a1 = l0 * l2 * l3;
+					double a2 = l0 * l1 * l4;
+					double a3 = l1 * l2 * l5;
+					double a4 = l3 * l4 * l5;
+
+					double a = std::max({a1, a2, a3, a4, J});
+					quality(i) = J * sqrt(2) / a;
+				}
+			}
+		}
+	} // namespace
+
 	class AMIPSForm : public ParametrizationForm
 	{
 	public:
@@ -97,6 +175,38 @@ namespace polyfem::solver
 			amips_energy_.assemble_grad(state_.mesh->is_volume(), state_.n_bases, state_.geom_bases(), state_.geom_bases(), state_.ass_vals_cache, 0, x, Eigen::VectorXd(), grad);
 			assert(grad.cols() == 1);
 			gradv = grad;
+		}
+
+		inline bool is_step_valid_with_param(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) const override
+		{
+			Eigen::MatrixXd V0;
+			Eigen::MatrixXi F;
+			state_.get_vf(V0, F);
+			assert((V0 - utils::unflatten(x0, state_.mesh->dimension())).norm() < 1e-12);
+			Eigen::MatrixXd V1 = utils::unflatten(x1, state_.mesh->dimension());
+			return is_flipped(V1, F);
+		}
+
+		inline static bool is_flipped(const Eigen::MatrixXd &V, const Eigen::MatrixXi &F)
+		{
+			if (F.cols() == 3)
+			{
+				for (int i = 0; i < F.rows(); i++)
+					if (triangle_jacobian(V.row(F(i, 0)), V.row(F(i, 1)), V.row(F(i, 2))) <= 0)
+						return true;
+			}
+			else if (F.cols() == 4)
+			{
+				for (int i = 0; i < F.rows(); i++)
+					if (tet_determinant(V.row(F(i, 0)), V.row(F(i, 1)), V.row(F(i, 2)), V.row(F(i, 3))) <= 0)
+						return true;
+			}
+			else
+			{
+				return true;
+			}
+
+			return false;
 		}
 
 	private:
