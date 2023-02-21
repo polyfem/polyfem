3bd37cff5eb61ac53803b97df55fc6101f9a1cd9
Huangzizhou <zizhou@nyu.edu>
2023-02-20 22:52:20 -0500

    changes in graph parameter

--- src/polyfem/solver/GraphParameter.cpp
+++ src/polyfem/solver/GraphParameter.cpp
@@ -3,6 +3,7 @@
 #include <polyfem/utils/MaybeParallelFor.hpp>
 #include <polyfem/mesh/mesh2D/Mesh2D.hpp>
 #include <polyfem/mesh/mesh3D/Mesh3D.hpp>
+#include <polyfem/mesh/GeometryReader.hpp>
 
 #include <sstream>
 
@@ -10,7 +11,7 @@ namespace polyfem
 {
     namespace {
         template <typename T>
-        std::string to_string_with_precision(const T a_value, const int n = 6)
+        std::string to_string_with_precision(const T a_value, const int n)
         {
             std::ostringstream out;
             out.precision(n);
@@ -64,14 +65,13 @@ namespace polyfem
 
         // periodic pattern
         unit_size_ = args["unit_size"];
-        compute_pattern_period();
+        // compute_pattern_period();
         
         pre_solve(initial_guess_);
     }
 
     void GraphParameter::compute_pattern_period()
     {
-        const mesh::Mesh &mesh = *(get_state().mesh);
         int elem_period_ = 0;
         full_to_periodic_.clear();
 
@@ -83,45 +83,47 @@ namespace polyfem
             
             return;
         }
-        
+
         RowVectorNd min, max;
-        mesh.bounding_box(min, max);
+        auto mesh = mesh::read_fem_mesh(get_state().args["geometry"][0], get_state().args["root_path"].get<std::string>());
+        const int n_elem_of_graph_mesh_ = mesh->n_elements();
+        mesh->bounding_box(min, max);
 
         Eigen::VectorXi nums;
-        nums.setZero(mesh.dimension());
-        for (int d = 0; d < mesh.dimension(); d++)
+        nums.setZero(mesh->dimension());
+        for (int d = 0; d < mesh->dimension(); d++)
         {
             int tmp = std::lround((max(d) - min(d)) / unit_size_);
             if (abs(tmp * unit_size_ + min(d) - max(d)) > 1e-8)
-                log_and_throw_error("Mesh size is not periodic!");
+                log_and_throw_error("Mesh size is not periodic! min: {}, max: {}, unit: {}", min.transpose(), max.transpose(), unit_size_);
             nums(d) = tmp;
         }
 
-        for (int e = 0; e < mesh.n_elements(); e++)
+        for (int e = 0; e < n_elem_of_graph_mesh_; e++)
         {
-            if ((get_barycenter(mesh, e) - min).maxCoeff() >= unit_size_)
+            if ((get_barycenter(*mesh, e) - min).maxCoeff() >= unit_size_)
             {
                 elem_period_ = e;
                 break;
             }
         }
         if (elem_period_ == 0)
-            elem_period_ = mesh.n_elements();
+            elem_period_ = n_elem_of_graph_mesh_;
 
         // node correspondence
         {
-            full_to_periodic_.assign(get_state().n_geom_bases, -1);
+            full_to_periodic_.assign(mesh->n_vertices(), -1);
 
-            utils::maybe_parallel_for(mesh.n_elements(), [&](int start, int end, int thread_id) {
-                for (int e = start; e < end; e++)
+            // utils::maybe_parallel_for(n_elem_of_graph_mesh_, [&](int start, int end, int thread_id) {
+                for (int e = 0; e < n_elem_of_graph_mesh_; e++)
                 {
-                    RowVectorNd offset = get_barycenter(mesh, e) - get_barycenter(mesh, e % elem_period_);
+                    RowVectorNd offset = get_barycenter(*mesh, e) - get_barycenter(*mesh, e % elem_period_);
                     
-                    assert(!mesh.is_volume());
-                    for (int lv = 0; lv < mesh.n_face_vertices(e); lv++) // only 2D
+                    assert(!mesh->is_volume());
+                    for (int lv = 0; lv < mesh->n_face_vertices(e); lv++) // only 2D
                     {
-                        int vid1 = mesh.face_vertex(e, lv);
-                        auto p1 = mesh.point(vid1);
+                        int vid1 = mesh->face_vertex(e, lv);
+                        auto p1 = mesh->point(vid1);
                         bool flag = false;
                         
                         if (e < elem_period_)
@@ -133,10 +135,10 @@ namespace polyfem
                         {
                             double min_diff = 1e5;
                             int min_id = -1;
-                            for (int lv2 = 0; lv2 < mesh.n_face_vertices(e % elem_period_); lv2++)
+                            for (int lv2 = 0; lv2 < mesh->n_face_vertices(e % elem_period_); lv2++)
                             {
-                                int vid2 = mesh.face_vertex(e % elem_period_, lv2);
-                                auto p2 = mesh.point(vid2);
+                                int vid2 = mesh->face_vertex(e % elem_period_, lv2);
+                                auto p2 = mesh->point(vid2);
 
                                 if ((p1 - offset - p2).norm() < min_diff)
                                 {
@@ -150,7 +152,7 @@ namespace polyfem
                         }
                     }
                 }
-            });
+            // });
         }
         
         logger().info("Number of elements in one period: {}, number of periods: {}", elem_period_, nums.prod());
@@ -226,7 +228,7 @@ namespace polyfem
 
         if (unit_size_ > 0)
         {
-            command = "python ../tile.py " + out_msh_path_;
+            command = "python tile.py " + out_msh_path_;
             try 
             {
                 return_val = system(command.c_str());
@@ -254,7 +256,7 @@ namespace polyfem
         // reload mesh and recompute basis
         for (auto state : states_ptr_)
         {
-            assert(state->in_args["geometry"].size() == 1);
+            // assert(state->in_args["geometry"].size() == 1);
             state->in_args["geometry"][0]["mesh"] = out_msh_path_;
 
 			state->mesh.reset();
@@ -262,7 +264,8 @@ namespace polyfem
 			state->assembler.update_lame_params(Eigen::MatrixXd(), Eigen::MatrixXd());
 
 			json in_args = state->in_args;
-			std::cout << in_args << std::endl;
+			// std::cout << in_args << std::endl;
+            in_args["output"]["log"]["level"] = 4;
 			state->init(in_args, false);
 
 			state->load_mesh();
@@ -270,6 +273,8 @@ namespace polyfem
 			state->build_basis();
 
             full_dim_ = state->n_geom_bases * state->mesh->dimension();
+
+            state->args["output"]["log"]["level"] = cur_log;
         }
 
         states_ptr_[0]->set_log_level(static_cast<spdlog::level::level_enum>(cur_log)); // log level is global, only need to change in one state
@@ -287,43 +292,62 @@ namespace polyfem
         std::vector<std::vector<double>> node_data;
         io::MshReader::load(out_velocity_path_, vertices, cells, elements, weights, body_ids, node_data_name, node_data);
 
+        vertices.array() += 1.0;
+        vertices.array() *= 0.5;
+
+        MatrixNd A;
+        VectorNd b;
+        {
+            const VectorNd mesh_dimensions = (vertices.colwise().maxCoeff() - vertices.colwise().minCoeff()).cwiseAbs();
+            mesh::construct_affine_transformation(get_state().args["geometry"][0]["transformation"], mesh_dimensions, A, b);
+            vertices = vertices * A.transpose();
+            vertices.rowwise() += b.transpose();
+        }
+        A.array() *= 0.5;
+
         for (int j = 0; j < vertices.rows(); j++)
         {
             auto v1 = vertices.row(j);
             auto v2 = get_state().mesh->point(j);
+
             if ((v1 - v2).norm() > 1e-6)
-                log_and_throw_error("Inconsistent reduced mesh and full mesh, diff = {}!", (v1 - v2).norm());
+                log_and_throw_error("Inconsistent reduced mesh and full mesh, v1 = {}, v2 = {}!", v1, v2);
         }
         
         assert(node_data_name.size() == node_data.size());
         shape_velocity_.setZero(get_state().n_geom_bases * dim, node_data.size() - 1);
         const auto &primitive_to_node = get_state().iso_parametric() ? get_state().mesh_nodes->primitive_to_node() : get_state().geom_mesh_nodes->primitive_to_node();
-        for (int j = 0; j < get_state().n_geom_bases; j++) // j is full vertex id
+        for (int j = 0; j < full_to_periodic_.size(); j++) // j is full vertex id
         {
-            if (primitive_to_node[j] >= 0 && primitive_to_node[j] < get_state().n_geom_bases)
+            if (primitive_to_node[j] >= 0 && primitive_to_node[j] < get_state().n_geom_bases) // primitive_to_node[j] is gbases id
+            {
                 for (int i = 1; i < node_data_name.size(); i++)
                     for (int d = 0; d < dim; d++)
                         shape_velocity_(primitive_to_node[j] * dim + d, i - 1) = node_data[0][full_to_periodic_[j] * 3 + d] * node_data[i][full_to_periodic_[j]];
+
+                Eigen::MatrixXd tmp = shape_velocity_.block(primitive_to_node[j] * dim, 0, dim, shape_velocity_.cols());
+                shape_velocity_.block(primitive_to_node[j] * dim, 0, dim, shape_velocity_.cols()) = A * tmp;
+            }
         }
 
-        // for (int i = 1; i < node_data_name.size(); i++) // debug shape velocity
-        // {
-        //     static int idx = 0;
-        //     std::vector<io::SolutionFrame> solution_frames;
-        //     get_state().out_geom.export_data(
-        //         get_state(),
-        //         shape_velocity_.col(i - 1), Eigen::MatrixXd(),
-        //         !get_state().args["time"].is_null(),
-        //         0, 0,
-        //         io::OutGeometryData::ExportOptions(get_state().args, get_state().mesh->is_linear(), get_state().problem->is_scalar(), get_state().solve_export_to_file),
-        //         "shape_vel_" + std::to_string(idx) + ".vtu",
-        //         "", // nodes_path,
-        //         "", // solution_path,
-        //         "", // stress_path,
-        //         "", // mises_path,
-        //         get_state().is_contact_enabled(), solution_frames);
-        //     idx++;
-        // }
+        for (int i = 1; i < node_data_name.size(); i++) // debug shape velocity
+        {
+            // static int idx = 0;
+            std::vector<io::SolutionFrame> solution_frames;
+            get_state().out_geom.export_data(
+                get_state(),
+                shape_velocity_.col(i - 1), Eigen::MatrixXd(),
+                !get_state().args["time"].is_null(),
+                0, 0,
+                io::OutGeometryData::ExportOptions(get_state().args, get_state().mesh->is_linear(), get_state().problem->is_scalar(), get_state().solve_export_to_file),
+                "shape_vel_" + std::to_string(i) + ".vtu",
+                "", // nodes_path,
+                "", // solution_path,
+                "", // stress_path,
+                "", // mises_path,
+                get_state().is_contact_enabled(), solution_frames);
+            // idx++;
+        }
         
         return true;
     }
