3b6086b8de04f9e4aa7c7e80f0e68f254e00e7cb
Arvi Gjoka <ag4571@nyu.edu>
2023-03-01 15:01:43 -0500

    Fix collision barrier form for opt

--- src/polyfem/solver/forms/adjoint_forms/BarrierForms.hpp
+++ src/polyfem/solver/forms/adjoint_forms/BarrierForms.hpp
@@ -7,38 +7,87 @@
 
 namespace polyfem::solver
 {
-	class CollisionBarrierForm : public ParametrizationForm
+	class CollisionBarrierForm : public AdjointForm
 	{
 	public:
-		CollisionBarrierForm(const CompositeParametrization &parametrizations, const State &state, const double dhat) : ParametrizationForm(parametrizations), state_(state), dhat_(dhat)
+		CollisionBarrierForm(const std::vector<std::shared_ptr<VariableToSimulation>> variable_to_simulation, const State &state, const double dhat) : AdjointForm(variable_to_simulation), state_(state), dhat_(dhat)
 		{
-			state.build_collision_mesh(collision_mesh_, state.n_geom_bases, state.geom_bases());
+			state_.build_collision_mesh(collision_mesh_, state.n_geom_bases, state_.geom_bases());
 
-			broad_phase_method = ipc::BroadPhaseMethod::HASH_GRID;
+			Eigen::MatrixXd V;
+			Eigen::MatrixXi F;
+			state_.get_vf(V, F);
+			X_init = utils::flatten(V);
+
+			broad_phase_method_ = ipc::BroadPhaseMethod::HASH_GRID;
 		}
 
-		double value_unweighted_with_param(const Eigen::VectorXd &x) const override
+		double value_unweighted(const Eigen::VectorXd &x) const override
 		{
-			assert(x.size() == state_.mesh->n_elements());
-
-			const Eigen::MatrixXd displaced_surface = collision_mesh_.vertices(utils::unflatten(x, state_.mesh->dimension()));
+			const Eigen::MatrixXd displaced_surface = collision_mesh_.vertices(utils::unflatten(get_updated_mesh_nodes(x), state_.mesh->dimension()));
 
 			return ipc::compute_barrier_potential(collision_mesh_, displaced_surface, constraint_set, dhat_);
 		}
 
-		void first_derivative_unweighted_with_param(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) const override
+		void compute_partial_gradient_unweighted(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) const override
 		{
-			assert(x.size() == state_.mesh->n_elements());
+			const Eigen::MatrixXd displaced_surface = collision_mesh_.vertices(utils::unflatten(get_updated_mesh_nodes(x), state_.mesh->dimension()));
 
-			const Eigen::MatrixXd displaced_surface = collision_mesh_.vertices(utils::unflatten(x, state_.mesh->dimension()));
+			Eigen::VectorXd grad = collision_mesh_.to_full_dof(ipc::compute_barrier_potential_gradient(collision_mesh_, displaced_surface, constraint_set, dhat_));
 
-			gradv = collision_mesh_.to_full_dof(ipc::compute_barrier_potential_gradient(collision_mesh_, displaced_surface, constraint_set, dhat_));
+			gradv.setZero(x.size());
+			for (auto &p : variable_to_simulations_)
+			{
+				if (&p->get_state() != &state_)
+					continue;
+				if (p->get_parameter_type() != ParameterType::Shape)
+					continue;
+				gradv += p->get_parametrization().apply_jacobian(grad, x);
+			}
 		}
 
-		void solution_changed_with_param(const Eigen::VectorXd &x) override
+		void solution_changed(const Eigen::VectorXd &x) override
 		{
-			const Eigen::MatrixXd displaced_surface = collision_mesh_.vertices(utils::unflatten(x, state_.mesh->dimension()));
+			const Eigen::MatrixXd displaced_surface = collision_mesh_.vertices(utils::unflatten(get_updated_mesh_nodes(x), state_.mesh->dimension()));
 			build_constraint_set(displaced_surface);
+		}
+
+		Eigen::MatrixXd compute_adjoint_rhs_unweighted(const Eigen::VectorXd &x, const State &state) override
+		{
+			return Eigen::MatrixXd::Zero(state.ndof(), state.diff_cached.size());
+		}
+
+		bool is_step_collision_free(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) const override
+		{
+			const Eigen::MatrixXd V0 = utils::unflatten(get_updated_mesh_nodes(x0), state_.mesh->dimension());
+			const Eigen::MatrixXd V1 = utils::unflatten(get_updated_mesh_nodes(x1), state_.mesh->dimension());
+
+			// Skip CCD if the displacement is zero.
+			if ((V1 - V0).lpNorm<Eigen::Infinity>() == 0.0)
+				return true;
+
+			bool is_valid = ipc::is_step_collision_free(
+				collision_mesh_,
+				collision_mesh_.vertices(V0),
+				collision_mesh_.vertices(V1),
+				broad_phase_method_,
+				1e-6, 1e6);
+
+			return is_valid;
+		}
+
+		double max_step_size(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) const override
+		{
+			const Eigen::MatrixXd V0 = utils::unflatten(get_updated_mesh_nodes(x0), state_.mesh->dimension());
+			const Eigen::MatrixXd V1 = utils::unflatten(get_updated_mesh_nodes(x1), state_.mesh->dimension());
+
+			double max_step = ipc::compute_collision_free_stepsize(
+				collision_mesh_,
+				collision_mesh_.vertices(V0),
+				collision_mesh_.vertices(V1),
+				broad_phase_method_, 1e-6, 1e6);
+
+			return max_step;
 		}
 
 	private:
@@ -48,17 +97,38 @@ namespace polyfem::solver
 			if (cached_displaced_surface.size() == displaced_surface.size() && cached_displaced_surface == displaced_surface)
 				return;
 
-			constraint_set.build(collision_mesh_, displaced_surface, dhat_, 0, broad_phase_method);
+			constraint_set.build(collision_mesh_, displaced_surface, dhat_, 0, broad_phase_method_);
 
 			cached_displaced_surface = displaced_surface;
 		}
 
+		Eigen::VectorXd get_updated_mesh_nodes(const Eigen::VectorXd &x) const
+		{
+			Eigen::VectorXd X = X_init;
+
+			for (auto &p : variable_to_simulations_)
+			{
+				if (&p->get_state() != &state_)
+					continue;
+				if (p->get_parameter_type() != ParameterType::Shape)
+					continue;
+				auto state_variable = p->get_parametrization().eval(x);
+				auto output_indexing = p->get_parametrization().get_output_indexing(x);
+				for (int i = 0; i < output_indexing.size(); ++i)
+					X(output_indexing(i)) = state_variable(i);
+			}
+
+			return X;
+		}
+
 		const State &state_;
+
+		Eigen::VectorXd X_init;
 
 		ipc::CollisionMesh collision_mesh_;
 		ipc::Constraints constraint_set;
 		const double dhat_;
-		ipc::BroadPhaseMethod broad_phase_method;
+		ipc::BroadPhaseMethod broad_phase_method_;
 	};
 
 	// class LayerThicknessForm : public ParametrizationForm
