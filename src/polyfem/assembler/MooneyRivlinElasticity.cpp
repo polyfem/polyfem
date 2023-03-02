219bb500c0424d181917aef1ca24596967ab779c
Teseo Schneider <teseo@bluewin.ch>
2023-03-02 12:58:25 -0800

    int division

--- src/polyfem/assembler/MooneyRivlinElasticity.cpp
+++ src/polyfem/assembler/MooneyRivlinElasticity.cpp
@@ -31,7 +31,7 @@ namespace polyfem::assembler
 		const T J = polyfem::utils::determinant(def_grad);
 		const T log_J = log(J);
 
-		const auto F_tilde = def_grad / pow(J, 1 / size());
+		const auto F_tilde = def_grad / pow(J, 1.0 / size());
 		const auto C_tilde = F_tilde * F_tilde.transpose();
 		const auto I1_tilde = first_invariant(C_tilde);
 		const auto I2_tilde = second_invariant(C_tilde);
