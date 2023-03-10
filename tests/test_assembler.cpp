778e7fdc70c9d3c9a6af41aaae606672dada341f
Teseo Schneider <teseo@bluewin.ch>
2023-03-10 09:58:15 -0800

    fixed merge

--- tests/test_assembler.cpp
+++ tests/test_assembler.cpp
@@ -94,7 +94,7 @@ TEST_CASE("hessian_hooke", "[assembler]")
 
 	state.assemble_stiffness_mat();
 
-	SpareMatrixCache mat_cache;
+	SparseMatrixCache mat_cache;
 	StiffnessMatrix hessian;
 	Eigen::MatrixXd disp(state.n_bases * 2, 1);
 	disp.setZero();
