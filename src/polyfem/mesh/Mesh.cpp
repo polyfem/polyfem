f2437fe4b3ec9bcf02a6f06cf2407b5383e1d15f
Zachary Ferguson <zfergus@nyu.edu>
2023-04-19 10:49:52 -0400

    Fix default boundary_ids_ in Mesh::append (#211)

--- src/polyfem/mesh/Mesh.cpp
+++ src/polyfem/mesh/Mesh.cpp
@@ -523,7 +523,7 @@ namespace polyfem::mesh
 		{
 			boundary_ids_.resize(n_boundary_elements());
 			for (int i = 0; i < boundary_ids_.size(); ++i)
-				boundary_ids_[i] = get_boundary_id(i); // results in default if boundary_ids_ is empty
+				boundary_ids_[i] = get_default_boundary_id(i);
 		}
 
 		if (mesh.has_boundary_ids())
@@ -534,7 +534,7 @@ namespace polyfem::mesh
 		{
 			boundary_ids_.resize(n_boundary_elements() + mesh.n_boundary_elements());
 			for (int i = 0; i < mesh.n_boundary_elements(); ++i)
-				boundary_ids_[n_boundary_elements() + i] = mesh.get_boundary_id(i); // results in default if boundary_ids_ is empty
+				boundary_ids_[n_boundary_elements() + i] = mesh.get_boundary_id(i); // results in default if mesh.boundary_ids_ is empty
 		}
 
 		// --------------------------------------------------------------------
