e6c172c8bd14d5adce5383a169b1bd74a8793e0e
Zachary Ferguson <zfergus@nyu.edu>
2023-04-18 21:57:37 -0400

    Curiously Recurring Template Pattern for GenericElastic (#210)
    * Implement GenericElastic autodiff using CRTP

--- src/polyfem/assembler/MooneyRivlinElasticity.hpp
+++ src/polyfem/assembler/MooneyRivlinElasticity.hpp
@@ -6,7 +6,7 @@
 // non linear MooneyRivlin material model
 namespace polyfem::assembler
 {
-	class MooneyRivlinElasticity : public GenericElastic
+	class MooneyRivlinElasticity : public GenericElastic<MooneyRivlinElasticity>
 	{
 	public:
 		MooneyRivlinElasticity();
@@ -21,18 +21,32 @@ namespace polyfem::assembler
 		std::string name() const override { return "MooneyRivlin"; }
 		std::map<std::string, ParamFunc> parameters() const override;
 
-		// This macro defines the overriden functions that compute the energy:
-		// template <typename T>
-		// T elastic_energy(const RowVectorNd &p, const int el_id, const DefGradMatrix<T> &def_grad) const override { elastic_energy_T<T>(p, el_id, def_grad); };
-		POLYFEM_OVERRIDE_ELASTIC_ENERGY
-
-	private:
 		template <typename T>
-		T elastic_energy_T(
+		T elastic_energy(
 			const RowVectorNd &p,
 			const int el_id,
-			const DefGradMatrix<T> &def_grad) const;
+			const DefGradMatrix<T> &def_grad) const
+		{
+			const double t = 0; // TODO
 
+			const double c1 = c1_(p, t, el_id);
+			const double c2 = c2_(p, t, el_id);
+			const double k = k_(p, t, el_id);
+
+			const T J = polyfem::utils::determinant(def_grad);
+			const T log_J = log(J);
+
+			const auto F_tilde = def_grad / pow(J, 1.0 / size());
+			const auto C_tilde = F_tilde * F_tilde.transpose();
+			const auto I1_tilde = first_invariant(C_tilde);
+			const auto I2_tilde = second_invariant(C_tilde);
+
+			const T val = c1 * (I1_tilde - size()) + c2 * (I2_tilde - size()) + k / 2 * (log_J * log_J);
+
+			return val;
+		}
+
+	private:
 		GenericMatParam c1_;
 		GenericMatParam c2_;
 		GenericMatParam k_;
