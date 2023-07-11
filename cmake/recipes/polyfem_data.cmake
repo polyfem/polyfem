7b22ed756ec559a85d88026bb34169da7bc9adc5
Huangzizhou <zizhou@nyu.edu>
2023-07-11 10:47:07 -0400

    fix operator splitting method

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -27,7 +27,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-        GIT_TAG 6351dc4c70b4dd543c9780893830197ac17a44ac
+        GIT_TAG d876c3e5981fcbf198357a632a0d4d16541d7e75
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
