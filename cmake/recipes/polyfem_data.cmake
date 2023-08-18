f55847b875531aba277ed284291316c8e732f10e
Teseo Schneider <teseo@bluewin.ch>
2023-08-18 16:24:29 -0700

    fixing amips test

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -27,7 +27,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-        GIT_TAG 0d0afb822e87f4dea9cf877082ae01da84ef43ce
+        GIT_TAG 5c45fc3
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
