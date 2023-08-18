7cf8b6e79a2b8f97ee606ca5913030a7da23b859
Zachary Ferguson <zfergus@nyu.edu>
2023-08-18 13:24:55 -0700

    Fix upsample mesh test

--- tests/test_upsample_mesh.cpp
+++ tests/test_upsample_mesh.cpp
@@ -9,7 +9,7 @@ TEST_CASE("upsample mesh", "[upsample_mesh]")
 {
 	Eigen::MatrixXd V;
 	Eigen::MatrixXi F;
-	REQUIRE(igl::readPLY("/Users/zachary/Desktop/octocat-coarse.ply", V, F));
+	REQUIRE(igl::readPLY(std::string(POLYFEM_DATA_DIR) + "/octocat-coarse.ply", V, F));
 
 	const double max_edge_length = 0.1 * polyfem::mesh::max_edge_length(V, F);
 
@@ -17,13 +17,19 @@ TEST_CASE("upsample mesh", "[upsample_mesh]")
 	Eigen::MatrixXi F_grid;
 	polyfem::mesh::regular_grid_tessilation(V, F, max_edge_length, V_grid, F_grid);
 
-	REQUIRE(igl::writePLY("/Users/zachary/Desktop/octocat-regular-tessilation.ply", V_grid, F_grid));
+	CHECK(V_grid.rows() == 126802);
+	CHECK(F_grid.rows() == 253600);
+
+	// REQUIRE(igl::writePLY("octocat-regular-tessilation.ply", V_grid, F_grid));
 
 #ifdef POLYFEM_WITH_TRIANGLE
 	Eigen::MatrixXd V_irregular;
 	Eigen::MatrixXi F_irregular;
 	polyfem::mesh::irregular_tessilation(V, F, max_edge_length, V_irregular, F_irregular);
 
-	REQUIRE(igl::writePLY("/Users/zachary/Desktop/octocat-irregular-tessilation.ply", V_irregular, F_irregular));
+	CHECK(V_irregular.rows() == 12645);
+	CHECK(F_irregular.rows() == 25286);
+
+	// REQUIRE(igl::writePLY("octocat-irregular-tessilation.ply", V_irregular, F_irregular));
 #endif
 }
