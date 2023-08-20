ad2c69da98467b526bc7c46f92fbeb8b68fe4653
Zachary Ferguson <zfergus@nyu.edu>
2023-08-19 18:32:36 -0700

    Switch to CPM (#239)
    * Switch to CPM
    * Update Catch
    * Update IPC Toolkit to v1.1.1
    * Drop TinyXML as it is not used in PolyFEM anymore

--- cmake/recipes/cppoptlib.cmake
+++ cmake/recipes/cppoptlib.cmake
@@ -1,4 +1,4 @@
-# CppNumericalSolvers
+# CppNumericalSolvers (https://github.com/PatWie/CppNumericalSolvers.git)
 # License: MIT
 
 if(TARGET cppoptlib)
@@ -7,16 +7,13 @@ endif()
 
 message(STATUS "Third-party: creating target 'cppoptlib'")
 
-
-include(FetchContent)
-FetchContent_Declare(
-    cppoptlib
-    GIT_REPOSITORY https://github.com/PatWie/CppNumericalSolvers.git
+include(CPM)
+CPMAddPackage(
+    NAME cppoptlib
+    GITHUB_REPOSITORY PatWie/CppNumericalSolvers
     GIT_TAG 7eddf28fa5a8872a956d3c8666055cac2f5a535d
-    GIT_SHALLOW FALSE
+    DOWNLOAD_ONLY TRUE
 )
-FetchContent_MakeAvailable(cppoptlib)
-
 
 add_library(cppoptlib INTERFACE)
 target_include_directories(cppoptlib SYSTEM INTERFACE ${cppoptlib_SOURCE_DIR}/include)
--- tests/verify_run.cpp
+++ tests/verify_run.cpp
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 #include "polyfem/utils/JSONUtils.hpp"
 #include "polyfem/State.hpp"
