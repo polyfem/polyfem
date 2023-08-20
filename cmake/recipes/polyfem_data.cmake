6a28cbf6b3e9961108a5b5bd899097d56f1caf2a
Zachary Ferguson <zfergus@nyu.edu>
2023-08-19 22:26:03 -0700

    Update data

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -27,7 +27,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-        GIT_TAG 447fff5
+        GIT_TAG 5c45fc3de0bc256e043bfb6517bfa7aad34dc4a6
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
