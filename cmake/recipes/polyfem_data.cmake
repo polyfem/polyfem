ce7fdfc2215e4f5cde0277cbc0a6ed29c248e375
Arvi Gjoka <ag4571@nyu.edu>
2023-11-03 11:56:15 -0400

    update polyfem data version

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -28,7 +28,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-        GIT_TAG 5a6235fb93dbe33b773defaed638f0c9c45ec8df
+        GIT_TAG 3ecab3e6026ef6a63897ddbb552bad1594928d9b
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
