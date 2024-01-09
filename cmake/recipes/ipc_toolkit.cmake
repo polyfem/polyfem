857c79387d4b08d3ded170651fdb12129c52354c
maxpaik16 <maxpaik@gmail.com>
2024-01-08 22:34:30 -0500

    add min_dist_ratio code

--- cmake/recipes/ipc_toolkit.cmake
+++ cmake/recipes/ipc_toolkit.cmake
@@ -8,4 +8,4 @@ endif()
 message(STATUS "Third-party: creating target 'ipc::toolkit'")
 
 include(CPM)
-CPMAddPackage("gh:geometryprocessing/smooth-ipc#f0e12ec5e21c8785eb030b810734744a9373cc05")
+CPMAddPackage("gh:geometryprocessing/smooth-ipc#2861e12886029dfbdb12afd5f87d02d05fcd6dcf")
