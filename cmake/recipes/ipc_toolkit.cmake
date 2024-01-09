7ee83c938fff3ac5911828b03b85846a15242672
maxpaik16 <maxpaik@gmail.com>
2024-01-09 11:45:01 -0500

    add adaptive epsilon flag

--- cmake/recipes/ipc_toolkit.cmake
+++ cmake/recipes/ipc_toolkit.cmake
@@ -8,4 +8,4 @@ endif()
 message(STATUS "Third-party: creating target 'ipc::toolkit'")
 
 include(CPM)
-CPMAddPackage("gh:geometryprocessing/smooth-ipc#2861e12886029dfbdb12afd5f87d02d05fcd6dcf")
+CPMAddPackage("gh:geometryprocessing/smooth-ipc#f0c2e26f642fc80f10a99d2089dd92605f37ad21")
