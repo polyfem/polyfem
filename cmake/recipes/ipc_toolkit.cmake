bed37e8c285308b530d99dae99737e554c696478
Huangzizhou <zizhou@nyu.edu>
2024-01-18 11:45:02 -0500

    update toolkit

--- cmake/recipes/ipc_toolkit.cmake
+++ cmake/recipes/ipc_toolkit.cmake
@@ -8,4 +8,4 @@ endif()
 message(STATUS "Third-party: creating target 'ipc::toolkit'")
 
 include(CPM)
-CPMAddPackage("gh:geometryprocessing/smooth-ipc#b0dcae55c86f9035d512131fa7fc6157b54e8e90")
+CPMAddPackage("gh:geometryprocessing/smooth-ipc#c9e88a02589023427aa07f08c5fcaad21f8073e5")
