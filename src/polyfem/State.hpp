2a1d73e8967ccae24b26c361568432fa5c8be3f5
arvigj <ag4571@nyu.edu>
2024-02-16 13:55:23 -0500

    remove unused matrix

--- src/polyfem/State.hpp
+++ src/polyfem/State.hpp
@@ -420,8 +420,6 @@ namespace polyfem
 		std::unordered_map<int, std::vector<mesh::LocalBoundary>> local_pressure_cavity;
 		/// nodes on the boundary of polygonal elements, used for harmonic bases
 		std::map<int, basis::InterfaceData> poly_edge_to_data;
-		/// Matrices containing the input per node dirichelt
-		std::vector<Eigen::MatrixXd> input_dirichlet;
 		/// per node dirichlet
 		std::vector<int> dirichlet_nodes;
 		std::vector<RowVectorNd> dirichlet_nodes_position;
