8c3d24aa67645cfaf9702f4a202e9a8b0bcc0a01
Huangzizhou <zizhou@nyu.edu>
2024-06-04 19:19:16 -0400

    try to fix unit test

--- tests/test_diff.cpp
+++ tests/test_diff.cpp
@@ -922,13 +922,13 @@ TEST_CASE("friction-contact", "[test_adjoint]")
 
 	auto nl_problem = std::make_shared<AdjointNLProblem>(obj, var2sim, states, opt_args);
 
-	Eigen::VectorXd velocity_discrete;
-	velocity_discrete.setRandom(1);
+	Eigen::VectorXd velocity_discrete(1);
+	velocity_discrete << 1.;
 
 	Eigen::VectorXd x(1);
 	x << 0.2;
 
-	verify_adjoint(*nl_problem, x, velocity_discrete, 1e-7, 1e-5);
+	verify_adjoint(*nl_problem, x, velocity_discrete, 1e-6, 1e-7);
 }
 
 TEST_CASE("barycenter", "[test_adjoint]")
