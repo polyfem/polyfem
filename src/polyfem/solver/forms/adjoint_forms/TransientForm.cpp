cbc7e9c77f0c1726042a59880a29f1602c5ff627
Huangzizhou <zizhou@nyu.edu>
2024-06-09 18:51:17 -0400

    remove parallelism in transient form

--- src/polyfem/solver/forms/adjoint_forms/TransientForm.cpp
+++ src/polyfem/solver/forms/adjoint_forms/TransientForm.cpp
@@ -1,35 +1,9 @@
 #include "TransientForm.hpp"
 #include <polyfem/State.hpp>
 #include <polyfem/io/MatrixIO.hpp>
-#include <polyfem/utils/MaybeParallelFor.hpp>
 
 namespace polyfem::solver
 {
-	namespace
-	{
-		class LocalThreadScalarStorage
-		{
-		public:
-			double val;
-
-			LocalThreadScalarStorage()
-			{
-				val = 0;
-			}
-		};
-
-		class LocalThreadMatStorage
-		{
-		public:
-			Eigen::MatrixXd mat;
-
-			LocalThreadMatStorage(const int row, const int col = 1)
-			{
-				mat.resize(row, col);
-				mat.setZero();
-			}
-		};
-	} // namespace
 	std::vector<double> TransientForm::get_transient_quadrature_weights() const
 	{
 		std::vector<double> weights;
@@ -78,23 +52,15 @@ namespace polyfem::solver
 	double TransientForm::value_unweighted(const Eigen::VectorXd &x) const
 	{
 		std::vector<double> weights = get_transient_quadrature_weights();
-		auto storage = utils::create_thread_storage(LocalThreadScalarStorage());
-
-		utils::maybe_parallel_for(time_steps_ + 1, [&](int start, int end, int thread_id) {
-			LocalThreadScalarStorage &local_storage = utils::get_local_thread_storage(storage, thread_id);
-
-			for (int i = start; i < end; i++)
-			{
-				if (weights[i] == 0)
-					continue;
-				const double tmp = obj_->value_unweighted_step(i, x);
-				local_storage.val += (weights[i] * obj_->weight()) * tmp;
-			}
-		});
 
 		double value = 0;
-		for (const LocalThreadScalarStorage &local_storage : storage)
-			value += local_storage.val;
+		for (int i = 0; i < time_steps_ + 1; i++)
+		{
+			if (weights[i] == 0)
+				continue;
+			const double tmp = obj_->value_unweighted_step(i, x);
+			value += (weights[i] * obj_->weight()) * tmp;
+		}
 
 		return value;
 	}
@@ -104,23 +70,14 @@ namespace polyfem::solver
 		terms.setZero(state.ndof(), time_steps_ + 1);
 		std::vector<double> weights = get_transient_quadrature_weights();
 
-		auto storage = utils::create_thread_storage(LocalThreadMatStorage(terms.rows(), terms.cols()));
-
-		utils::maybe_parallel_for(time_steps_ + 1, [&](int start, int end, int thread_id) {
-			LocalThreadMatStorage &local_storage = utils::get_local_thread_storage(storage, thread_id);
-
-			for (int i = start; i < end; i++)
-			{
-				if (weights[i] == 0)
-					continue;
-				local_storage.mat.col(i) = weights[i] * obj_->compute_adjoint_rhs_step(i, x, state);
-				if (obj_->depends_on_step_prev() && i > 0)
-					local_storage.mat.col(i - 1) = weights[i] * obj_->compute_adjoint_rhs_step_prev(i, x, state);
-			}
-		});
-
-		for (const LocalThreadMatStorage &local_storage : storage)
-			terms += local_storage.mat;
+		for (int i = 0; i < time_steps_ + 1; i++)
+		{
+			if (weights[i] == 0)
+				continue;
+			terms.col(i) = weights[i] * obj_->compute_adjoint_rhs_step(i, x, state);
+			if (obj_->depends_on_step_prev() && i > 0)
+				terms.col(i - 1) = weights[i] * obj_->compute_adjoint_rhs_step_prev(i, x, state);
+		}
 
 		return terms * weight();
 	}
@@ -128,23 +85,16 @@ namespace polyfem::solver
 	{
 		gradv.setZero(x.size());
 		std::vector<double> weights = get_transient_quadrature_weights();
-		auto storage = utils::create_thread_storage(LocalThreadMatStorage(gradv.size()));
 
-		utils::maybe_parallel_for(time_steps_ + 1, [&](int start, int end, int thread_id) {
-			LocalThreadMatStorage &local_storage = utils::get_local_thread_storage(storage, thread_id);
+		Eigen::VectorXd tmp;
+		for (int i = 0; i < time_steps_ + 1; i++)
+		{
+			if (weights[i] == 0)
+				continue;
+			obj_->compute_partial_gradient_step(i, x, tmp);
+			gradv += weights[i] * tmp;
+		}
 
-			Eigen::VectorXd tmp;
-			for (int i = start; i < end; i++)
-			{
-				if (weights[i] == 0)
-					continue;
-				obj_->compute_partial_gradient_step(i, x, tmp);
-				local_storage.mat += weights[i] * tmp;
-			}
-		});
-
-		for (const LocalThreadMatStorage &local_storage : storage)
-			gradv += local_storage.mat;
 		gradv *= weight();
 	}
 
