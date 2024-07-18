fe956f4ede42b8c8345cd08d3cb6cc9fd39d07a9
arvigj <ag4571@nyu.edu>
2024-07-18 14:00:31 +0000

    Deploying to gh-pages from @ polyfem/polyfem@22ad56bef79742bf1e6db1934e2144445a036664 🚀

--- classpolyfem_1_1assembler_1_1_elasticity_tensor.js
+++ classpolyfem_1_1assembler_1_1_elasticity_tensor.js
@@ -7,8 +7,8 @@ var classpolyfem_1_1assembler_1_1_elasticity_tensor =
     [ "set_from_entries", "classpolyfem_1_1assembler_1_1_elasticity_tensor.html#a4608e7068f3674b79be160ab8710208a", null ],
     [ "set_from_lambda_mu", "classpolyfem_1_1assembler_1_1_elasticity_tensor.html#ada68f03617fe78eb6fdac8d9e56733b5", null ],
     [ "set_from_young_poisson", "classpolyfem_1_1assembler_1_1_elasticity_tensor.html#a537d932b4528f2776e2a7d683c35e2a2", null ],
-    [ "set_orthotropic", "classpolyfem_1_1assembler_1_1_elasticity_tensor.html#a3a00af3c46c247c6a9d016d17e51d4a9", null ],
-    [ "set_orthotropic", "classpolyfem_1_1assembler_1_1_elasticity_tensor.html#ad81fbc66104711e6d3c8e1b4be98b6c7", null ],
+    [ "set_orthotropic", "classpolyfem_1_1assembler_1_1_elasticity_tensor.html#aa5e6bd2f36653155bb6bfdf87bdfa5ae", null ],
+    [ "set_orthotropic", "classpolyfem_1_1assembler_1_1_elasticity_tensor.html#a8b359bbbcc2fb27d6da470e18d9bbb03", null ],
     [ "size_", "classpolyfem_1_1assembler_1_1_elasticity_tensor.html#a395444fa7f5aa81652fd2d2318b9181c", null ],
     [ "stifness_tensor_", "classpolyfem_1_1assembler_1_1_elasticity_tensor.html#a7f0bbea9d26c229d860e62ecbe0de1b9", null ]
 ];
