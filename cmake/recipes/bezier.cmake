fd9d9d34288a4d95291cac1a2bfadbf159136282
fsichetti <f.sichetti@gmail.com>
2024-09-19 12:55:01 +0200

    fix

--- cmake/recipes/bezier.cmake
+++ cmake/recipes/bezier.cmake
@@ -19,4 +19,4 @@ if (EIGEN_INTERFACE)
 endif()
 
 include(CPM)
-CPMAddPackage("https://gitlab.com/fsichetti/robust-bezier-subdivision.git#495f74c2bcef3f69e55c08c543a11a0db349c166")
+CPMAddPackage("https://gitlab.com/fsichetti/robust-bezier-subdivision.git#25f2a7404aa82a9b89a4f579185f6d1e537c659f")
