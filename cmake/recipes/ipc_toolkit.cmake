1d383506da7537f6172131bf4d388a2abc31dbbd
maxpaik16 <maxpaik@gmail.com>
2024-10-02 17:18:01 -0400

    update ipc recipe

--- cmake/recipes/ipc_toolkit.cmake
+++ cmake/recipes/ipc_toolkit.cmake
@@ -8,4 +8,4 @@ endif()
 message(STATUS "Third-party: creating target 'ipc::toolkit'")
 
 include(CPM)
-CPMAddPackage("gh:ipc-sim/ipc-toolkit#617b80e92eb4310791a722fd1cc20c902b17683f")
+CPMAddPackage("gh:ipc-sim/ipc-toolkit#6138afafc1bb0045da35176b18252ee5f3811fd9")
