2453e4ee359f74cba8fa2d02d53f80b4895a6189
maxpaik16 <maxpaik@gmail.com>
2024-10-15 11:49:26 -0400

    Adhesion form scaffolding

---
+++ src/polyfem/solver/forms/NormalAdhesionForm.hpp
@@ -0,0 +1,197 @@
+#pragma once
+
+#include "Form.hpp"
+
+#include <polyfem/Common.hpp>
+#include <polyfem/utils/Types.hpp>
+
+#include <ipc/collisions/normal/normal_collisions.hpp>
+#include <ipc/collision_mesh.hpp>
+#include <ipc/broad_phase/broad_phase.hpp>
+#include <ipc/potentials/normal_adhesion_potential.hpp>
+
+// map BroadPhaseMethod values to JSON as strings
+namespace ipc
+{
+	NLOHMANN_JSON_SERIALIZE_ENUM(
+		ipc::BroadPhaseMethod,
+		{{ipc::BroadPhaseMethod::HASH_GRID, "hash_grid"}, // also default
+		 {ipc::BroadPhaseMethod::HASH_GRID, "HG"},
+		 {ipc::BroadPhaseMethod::BRUTE_FORCE, "brute_force"},
+		 {ipc::BroadPhaseMethod::BRUTE_FORCE, "BF"},
+		 {ipc::BroadPhaseMethod::SPATIAL_HASH, "spatial_hash"},
+		 {ipc::BroadPhaseMethod::SPATIAL_HASH, "SH"},
+		 {ipc::BroadPhaseMethod::BVH, "bvh"},
+		 {ipc::BroadPhaseMethod::BVH, "BVH"},
+		 {ipc::BroadPhaseMethod::SWEEP_AND_PRUNE, "sweep_and_prune"},
+		 {ipc::BroadPhaseMethod::SWEEP_AND_PRUNE, "SAP"},
+		 {ipc::BroadPhaseMethod::SWEEP_AND_TINIEST_QUEUE, "sweep_and_tiniest_queue"},
+		 {ipc::BroadPhaseMethod::SWEEP_AND_TINIEST_QUEUE, "STQ"}})
+} // namespace ipc
+
+namespace polyfem::solver
+{
+	/// @brief Form representing the contact potential and forces
+	class NormalAdhesionForm : public Form
+	{
+	public:
+		/// @brief Construct a new Contact Form object
+		/// @param collision_mesh Reference to the collision mesh
+		/// @param dhat Barrier activation distance
+		/// @param avg_mass Average mass of the mesh
+		/// @param use_adaptive_barrier_stiffness If true, use an adaptive barrier stiffness
+		/// @param is_time_dependent Is the simulation time dependent?
+		/// @param broad_phase_method Broad phase method to use for distance and CCD evaluations
+		/// @param ccd_tolerance Continuous collision detection tolerance
+		/// @param ccd_max_iterations Continuous collision detection maximum iterations
+		NormalAdhesionForm(const ipc::CollisionMesh &collision_mesh,
+					const double dhat,
+					const double avg_mass,
+					const bool use_convergent_formulation,
+					const bool use_adaptive_barrier_stiffness,
+					const bool is_time_dependent,
+					const bool enable_shape_derivatives,
+					const ipc::BroadPhaseMethod broad_phase_method,
+					const double ccd_tolerance,
+					const int ccd_max_iterations);
+		virtual ~NormalAdhesionForm() = default;
+
+		std::string name() const override { return "normal adhesion"; }
+
+		/// @brief Initialize the form
+		/// @param x Current solution
+		void init(const Eigen::VectorXd &x) override;
+
+		virtual void force_shape_derivative(const ipc::NormalCollisions &collision_set, const Eigen::MatrixXd &solution, const Eigen::VectorXd &adjoint_sol, Eigen::VectorXd &term);
+
+	protected:
+		/// @brief Compute the contact barrier potential value
+		/// @param x Current solution
+		/// @return Value of the contact barrier potential
+		virtual double value_unweighted(const Eigen::VectorXd &x) const override;
+
+		/// @brief Compute the value of the form multiplied per element
+		/// @param x Current solution
+		/// @return Computed value
+		Eigen::VectorXd value_per_element_unweighted(const Eigen::VectorXd &x) const override;
+
+		/// @brief Compute the first derivative of the value wrt x
+		/// @param[in] x Current solution
+		/// @param[out] gradv Output gradient of the value wrt x
+		virtual void first_derivative_unweighted(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) const override;
+
+		/// @brief Compute the second derivative of the value wrt x
+		/// @param x Current solution
+		/// @param hessian Output Hessian of the value wrt x
+		virtual void second_derivative_unweighted(const Eigen::VectorXd &x, StiffnessMatrix &hessian) const override;
+
+	public:
+		/// @brief Update time-dependent fields
+		/// @param t Current time
+		/// @param x Current solution at time t
+		void update_quantities(const double t, const Eigen::VectorXd &x) override;
+
+		/// @brief Determine the maximum step size allowable between the current and next solution
+		/// @param x0 Current solution (step size = 0)
+		/// @param x1 Next solution (step size = 1)
+		/// @return Maximum allowable step size
+		double max_step_size(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) const override;
+
+		/// @brief Initialize variables used during the line search
+		/// @param x0 Current solution
+		/// @param x1 Next solution
+		void line_search_begin(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) override;
+
+		/// @brief Clear variables used during the line search
+		void line_search_end() override;
+
+		/// @brief Update cached fields upon a change in the solution
+		/// @param new_x New solution
+		void solution_changed(const Eigen::VectorXd &new_x) override;
+
+		/// @brief Update fields after a step in the optimization
+		/// @param iter_num Optimization iteration number
+		/// @param x Current solution
+		void post_step(const polysolve::nonlinear::PostStepData &data) override;
+
+		/// @brief Checks if the step is collision free
+		/// @return True if the step is collision free else false
+		bool is_step_collision_free(const Eigen::VectorXd &x0, const Eigen::VectorXd &x1) const override;
+
+		/// @brief Update the barrier stiffness based on the current elasticity energy
+		/// @param x Current solution
+		virtual void update_barrier_stiffness(const Eigen::VectorXd &x, const Eigen::MatrixXd &grad_energy);
+
+		/// @brief Compute the displaced positions of the surface nodes
+		Eigen::MatrixXd compute_displaced_surface(const Eigen::VectorXd &x) const;
+
+		/// @brief Get the current barrier stiffness
+		double barrier_stiffness() const { return barrier_stiffness_; }
+		/// @brief Get the current barrier stiffness
+		void set_barrier_stiffness(const double barrier_stiffness) { barrier_stiffness_ = barrier_stiffness; }
+		/// @brief Get use_adaptive_barrier_stiffness
+		bool use_adaptive_barrier_stiffness() const { return use_adaptive_barrier_stiffness_; }
+		/// @brief Get use_convergent_formulation
+		bool use_convergent_formulation() const { return collision_set_.use_area_weighting() && collision_set_.use_improved_max_approximator(); }
+
+		bool enable_shape_derivatives() const { return enable_shape_derivatives_; }
+
+		double weight() const override { return weight_ * barrier_stiffness_; }
+
+		/// @brief If true, output debug files
+		bool save_ccd_debug_meshes = false;
+
+		double dhat() const { return dhat_; }
+		const ipc::NormalCollisions &collision_set() const { return collision_set_; }
+		const ipc::NormalAdhesionPotential &normal_adhesion_potential() const { return normal_adhesion_potential_; }
+
+	protected:
+		/// @brief Update the cached candidate set for the current solution
+		/// @param displaced_surface Vertex positions displaced by the current solution
+		void update_collision_set(const Eigen::MatrixXd &displaced_surface);
+
+		/// @brief Collision mesh
+		const ipc::CollisionMesh &collision_mesh_;
+
+		/// @brief Barrier activation distance
+		const double dhat_;
+
+		/// @brief Minimum distance between elements
+		const double dmin_ = 0;
+
+		/// @brief If true, use an adaptive barrier stiffness
+		const bool use_adaptive_barrier_stiffness_;
+		/// @brief Barrier stiffness
+		double barrier_stiffness_;
+		/// @brief Maximum barrier stiffness to use when using adaptive barrier stiffness
+		double max_barrier_stiffness_;
+
+		/// @brief Average mass of the mesh (used for adaptive barrier stiffness)
+		const double avg_mass_;
+
+		/// @brief Is the simulation time dependent?
+		const bool is_time_dependent_;
+
+		/// @brief Enable shape derivatives computation
+		const bool enable_shape_derivatives_;
+
+		/// @brief Broad phase method to use for distance and CCD evaluations
+		const ipc::BroadPhaseMethod broad_phase_method_;
+		/// @brief Continuous collision detection tolerance
+		const double ccd_tolerance_;
+		/// @brief Continuous collision detection maximum iterations
+		const int ccd_max_iterations_;
+
+		/// @brief Previous minimum distance between all elements
+		double prev_distance_;
+
+		/// @brief If true, use the cached candidate set for the current solution
+		bool use_cached_candidates_ = false;
+		/// @brief Cached constraint set for the current solution
+		ipc::NormalCollisions collision_set_;
+		/// @brief Cached candidate set for the current solution
+		ipc::Candidates candidates_;
+
+		const ipc::NormalAdhesionPotential normal_adhesion_potential_;
+	};
+} // namespace polyfem::solver
---
+++ src/polyfem/solver/forms/TangentialAdhesionForm.cpp
@@ -0,0 +1,152 @@
+#include "TangentialAdhesionForm.hpp"
+#include "ContactForm.hpp"
+
+#include <polyfem/utils/Timer.hpp>
+#include <polyfem/utils/MatrixUtils.hpp>
+
+namespace polyfem::solver
+{
+	FrictionForm::FrictionForm(
+		const ipc::CollisionMesh &collision_mesh,
+		const std::shared_ptr<time_integrator::ImplicitTimeIntegrator> time_integrator,
+		const double epsv,
+		const double mu,
+		const ipc::BroadPhaseMethod broad_phase_method,
+		const ContactForm &contact_form,
+		const int n_lagging_iters)
+		: collision_mesh_(collision_mesh),
+		  time_integrator_(time_integrator),
+		  epsv_(epsv),
+		  mu_(mu),
+		  broad_phase_method_(broad_phase_method),
+		  n_lagging_iters_(n_lagging_iters < 0 ? std::numeric_limits<int>::max() : n_lagging_iters),
+		  contact_form_(contact_form),
+		  friction_potential_(epsv)
+	{
+		assert(epsv_ > 0);
+	}
+
+	void FrictionForm::force_shape_derivative(
+		const Eigen::MatrixXd &prev_solution,
+		const Eigen::MatrixXd &solution,
+		const Eigen::MatrixXd &adjoint,
+		const ipc::TangentialCollisions &friction_constraints_set,
+		Eigen::VectorXd &term)
+	{
+		Eigen::MatrixXd U = collision_mesh_.vertices(utils::unflatten(solution, collision_mesh_.dim()));
+		Eigen::MatrixXd U_prev = collision_mesh_.vertices(utils::unflatten(prev_solution, collision_mesh_.dim()));
+
+		// TODO: use the time integration to compute the velocity
+		const Eigen::MatrixXd velocities = (U - U_prev) / time_integrator_->dt();
+
+		StiffnessMatrix hess = -friction_potential_.force_jacobian(
+			friction_constraints_set,
+			collision_mesh_, collision_mesh_.rest_positions(),
+			/*lagged_displacements=*/U_prev, velocities,
+			contact_form_.barrier_potential(),
+			contact_form_.barrier_stiffness(),
+			ipc::FrictionPotential::DiffWRT::REST_POSITIONS);
+
+		// {
+		// 	Eigen::MatrixXd X = collision_mesh_.rest_positions();
+		// 	Eigen::VectorXd x = utils::flatten(X);
+		// 	const double barrier_stiffness = contact_form_.barrier_stiffness();
+		// 	const double dhat = dhat_;
+		// 	const double mu = mu_;
+		// 	const double epsv = epsv_;
+		// 	const double dt = time_integrator_->dt();
+
+		// 	Eigen::MatrixXd fgrad;
+		// 	fd::finite_jacobian(
+		// 		x, [&](const Eigen::VectorXd &y) -> Eigen::VectorXd
+		// 		{
+		// 			Eigen::MatrixXd fd_X = utils::unflatten(y, X.cols());
+
+		// 			ipc::CollisionMesh fd_mesh(fd_X, collision_mesh_.edges(), collision_mesh_.faces());
+		// 			fd_mesh.init_area_jacobians();
+
+		// 			ipc::TangentialCollisions fd_friction_constraints;
+		// 			ipc::NormalCollisions fd_constraints;
+		// 			fd_constraints.set_use_convergent_formulation(contact_form_.use_convergent_formulation());
+		// 			fd_constraints.set_are_shape_derivatives_enabled(true);
+		// 			fd_constraints.build(fd_mesh, fd_X + U_prev, dhat);
+
+		// 			fd_friction_constraints.build(
+		// 				fd_mesh, fd_X + U_prev, fd_constraints, dhat, barrier_stiffness,
+		// 				mu);
+
+		// 			return fd_friction_constraints.compute_potential_gradient(fd_mesh, (U - U_prev) / dt, epsv);
+
+		// 		}, fgrad, fd::AccuracyOrder::SECOND, 1e-8);
+
+		// 	std::cout << "force shape derivative error " << (fgrad - hess).norm() << " " << hess.norm() << "\n";
+		// }
+
+		term = collision_mesh_.to_full_dof(hess).transpose() * adjoint;
+	}
+
+	Eigen::MatrixXd FrictionForm::compute_displaced_surface(const Eigen::VectorXd &x) const
+	{
+		return contact_form_.compute_displaced_surface(x);
+	}
+
+	Eigen::MatrixXd FrictionForm::compute_surface_velocities(const Eigen::VectorXd &x) const
+	{
+		// In the case of a static problem, the velocity is the displacement
+		const Eigen::VectorXd v = time_integrator_ != nullptr ? time_integrator_->compute_velocity(x) : x;
+		return collision_mesh_.map_displacements(utils::unflatten(v, collision_mesh_.dim()));
+	}
+
+	double FrictionForm::dv_dx() const
+	{
+		return time_integrator_ != nullptr ? time_integrator_->dv_dx() : 1;
+	}
+
+	double FrictionForm::value_unweighted(const Eigen::VectorXd &x) const
+	{
+		return friction_potential_(friction_collision_set_, collision_mesh_, compute_surface_velocities(x)) / dv_dx();
+	}
+
+	void FrictionForm::first_derivative_unweighted(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) const
+	{
+		const Eigen::VectorXd grad_friction = friction_potential_.gradient(
+			friction_collision_set_, collision_mesh_, compute_surface_velocities(x));
+		gradv = collision_mesh_.to_full_dof(grad_friction);
+	}
+
+	void FrictionForm::second_derivative_unweighted(const Eigen::VectorXd &x, StiffnessMatrix &hessian) const
+	{
+		POLYFEM_SCOPED_TIMER("friction hessian");
+
+		ipc::PSDProjectionMethod psd_projection_method; 
+
+		if (project_to_psd_) {
+			psd_projection_method = ipc::PSDProjectionMethod::CLAMP;
+		} else {
+			psd_projection_method = ipc::PSDProjectionMethod::NONE;
+		}
+
+		hessian = dv_dx() * friction_potential_.hessian( //
+					  friction_collision_set_, collision_mesh_, compute_surface_velocities(x), psd_projection_method);
+
+		hessian = collision_mesh_.to_full_dof(hessian);
+	}
+
+	void FrictionForm::update_lagging(const Eigen::VectorXd &x, const int iter_num)
+	{
+		const Eigen::MatrixXd displaced_surface = compute_displaced_surface(x);
+
+		ipc::NormalCollisions collision_set;
+		//collision_set.set_use_convergent_formulation(contact_form_.use_convergent_formulation());
+		collision_set.set_use_improved_max_approximator(contact_form_.use_convergent_formulation());
+		collision_set.set_use_area_weighting(contact_form_.use_convergent_formulation());
+		
+		collision_set.set_enable_shape_derivatives(contact_form_.enable_shape_derivatives());
+		collision_set.build(
+			collision_mesh_, displaced_surface, contact_form_.dhat(), /*dmin=*/0, broad_phase_method_);
+
+		friction_collision_set_.build(
+			collision_mesh_, displaced_surface, collision_set,
+			contact_form_.barrier_potential(), contact_form_.barrier_stiffness(), mu_);
+	}
+} // namespace polyfem::solver
