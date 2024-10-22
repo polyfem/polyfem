db1bffab740c4bbbf40ec5a217457cf0a47d85dc
maxpaik16 <maxpaik@gmail.com>
2024-10-22 15:39:26 -0400

    some (incomplete) tangential adhesion scaffolding

--- src/polyfem/solver/forms/TangentialAdhesionForm.cpp
+++ src/polyfem/solver/forms/TangentialAdhesionForm.cpp
@@ -6,7 +6,7 @@
 
 namespace polyfem::solver
 {
-	FrictionForm::FrictionForm(
+	TangentialAdhesionForm::TangentialAdhesionForm(
 		const ipc::CollisionMesh &collision_mesh,
 		const std::shared_ptr<time_integrator::ImplicitTimeIntegrator> time_integrator,
 		const double epsv,
@@ -21,12 +21,12 @@ namespace polyfem::solver
 		  broad_phase_method_(broad_phase_method),
 		  n_lagging_iters_(n_lagging_iters < 0 ? std::numeric_limits<int>::max() : n_lagging_iters),
 		  contact_form_(contact_form),
-		  friction_potential_(epsv)
+		  tangential_adhesion_potential_(epsv)
 	{
 		assert(epsv_ > 0);
 	}
 
-	void FrictionForm::force_shape_derivative(
+	void TangentialAdhesionForm::force_shape_derivative(
 		const Eigen::MatrixXd &prev_solution,
 		const Eigen::MatrixXd &solution,
 		const Eigen::MatrixXd &adjoint,
@@ -47,76 +47,41 @@ namespace polyfem::solver
 			contact_form_.barrier_stiffness(),
 			ipc::FrictionPotential::DiffWRT::REST_POSITIONS);
 
-		// {
-		// 	Eigen::MatrixXd X = collision_mesh_.rest_positions();
-		// 	Eigen::VectorXd x = utils::flatten(X);
-		// 	const double barrier_stiffness = contact_form_.barrier_stiffness();
-		// 	const double dhat = dhat_;
-		// 	const double mu = mu_;
-		// 	const double epsv = epsv_;
-		// 	const double dt = time_integrator_->dt();
-
-		// 	Eigen::MatrixXd fgrad;
-		// 	fd::finite_jacobian(
-		// 		x, [&](const Eigen::VectorXd &y) -> Eigen::VectorXd
-		// 		{
-		// 			Eigen::MatrixXd fd_X = utils::unflatten(y, X.cols());
-
-		// 			ipc::CollisionMesh fd_mesh(fd_X, collision_mesh_.edges(), collision_mesh_.faces());
-		// 			fd_mesh.init_area_jacobians();
-
-		// 			ipc::TangentialCollisions fd_friction_constraints;
-		// 			ipc::NormalCollisions fd_constraints;
-		// 			fd_constraints.set_use_convergent_formulation(contact_form_.use_convergent_formulation());
-		// 			fd_constraints.set_are_shape_derivatives_enabled(true);
-		// 			fd_constraints.build(fd_mesh, fd_X + U_prev, dhat);
-
-		// 			fd_friction_constraints.build(
-		// 				fd_mesh, fd_X + U_prev, fd_constraints, dhat, barrier_stiffness,
-		// 				mu);
-
-		// 			return fd_friction_constraints.compute_potential_gradient(fd_mesh, (U - U_prev) / dt, epsv);
-
-		// 		}, fgrad, fd::AccuracyOrder::SECOND, 1e-8);
-
-		// 	std::cout << "force shape derivative error " << (fgrad - hess).norm() << " " << hess.norm() << "\n";
-		// }
-
 		term = collision_mesh_.to_full_dof(hess).transpose() * adjoint;
 	}
 
-	Eigen::MatrixXd FrictionForm::compute_displaced_surface(const Eigen::VectorXd &x) const
+	Eigen::MatrixXd TangentialAdhesionForm::compute_displaced_surface(const Eigen::VectorXd &x) const
 	{
 		return contact_form_.compute_displaced_surface(x);
 	}
 
-	Eigen::MatrixXd FrictionForm::compute_surface_velocities(const Eigen::VectorXd &x) const
+	Eigen::MatrixXd TangentialAdhesionForm::compute_surface_velocities(const Eigen::VectorXd &x) const
 	{
 		// In the case of a static problem, the velocity is the displacement
 		const Eigen::VectorXd v = time_integrator_ != nullptr ? time_integrator_->compute_velocity(x) : x;
 		return collision_mesh_.map_displacements(utils::unflatten(v, collision_mesh_.dim()));
 	}
 
-	double FrictionForm::dv_dx() const
+	double TangentialAdhesionForm::dv_dx() const
 	{
 		return time_integrator_ != nullptr ? time_integrator_->dv_dx() : 1;
 	}
 
-	double FrictionForm::value_unweighted(const Eigen::VectorXd &x) const
+	double TangentialAdhesionForm::value_unweighted(const Eigen::VectorXd &x) const
 	{
 		return friction_potential_(friction_collision_set_, collision_mesh_, compute_surface_velocities(x)) / dv_dx();
 	}
 
-	void FrictionForm::first_derivative_unweighted(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) const
+	void TangentialAdhesionForm::first_derivative_unweighted(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) const
 	{
 		const Eigen::VectorXd grad_friction = friction_potential_.gradient(
 			friction_collision_set_, collision_mesh_, compute_surface_velocities(x));
 		gradv = collision_mesh_.to_full_dof(grad_friction);
 	}
 
-	void FrictionForm::second_derivative_unweighted(const Eigen::VectorXd &x, StiffnessMatrix &hessian) const
+	void TangentialAdhesionForm::second_derivative_unweighted(const Eigen::VectorXd &x, StiffnessMatrix &hessian) const
 	{
-		POLYFEM_SCOPED_TIMER("friction hessian");
+		POLYFEM_SCOPED_TIMER("tangential adhesion hessian");
 
 		ipc::PSDProjectionMethod psd_projection_method; 
 
@@ -132,7 +97,7 @@ namespace polyfem::solver
 		hessian = collision_mesh_.to_full_dof(hessian);
 	}
 
-	void FrictionForm::update_lagging(const Eigen::VectorXd &x, const int iter_num)
+	void TangentialAdhesionForm::update_lagging(const Eigen::VectorXd &x, const int iter_num)
 	{
 		const Eigen::MatrixXd displaced_surface = compute_displaced_surface(x);
 
@@ -145,7 +110,7 @@ namespace polyfem::solver
 		collision_set.build(
 			collision_mesh_, displaced_surface, contact_form_.dhat(), /*dmin=*/0, broad_phase_method_);
 
-		friction_collision_set_.build(
+		tangential_collision_set_.build(
 			collision_mesh_, displaced_surface, collision_set,
 			contact_form_.barrier_potential(), contact_form_.barrier_stiffness(), mu_);
 	}
