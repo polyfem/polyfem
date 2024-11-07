cc9f659aad11f184e5ed23a19d942b4ffffa42fb
ZizhouHuang <zizhou@nyu.edu>
2024-11-07 15:56:46 -0500

    update bezier

--- cmake/recipes/bezier.cmake
+++ cmake/recipes/bezier.cmake
@@ -20,4 +20,4 @@ if (EIGEN_INTERFACE)
 endif()
 
 include(CPM)
-CPMAddPackage("gh:Huangzizhou/bezier#270954f785acaafb75127c7cbc98758e1dd5da5f")
+CPMAddPackage("gh:Huangzizhou/bezier#087a62aa06a59dd900d4dc1e737fffd5662f9490")
