a01618139c282f433a544ac12f8076ed6f7739ff
teseoch <teseo.schneider@gmail.com>
2024-11-08 15:56:15 -0800

    new generic AL

--- src/polyfem/solver/forms/lagrangian/GenericLagrangianForm.cpp
+++ src/polyfem/solver/forms/lagrangian/GenericLagrangianForm.cpp
@@ -4,39 +4,32 @@
 
 namespace polyfem::solver
 {
-	GenericLagrangianForm::GenericLagrangianForm(const std::vector<int> &constraint_nodes,
-												 const StiffnessMatrix &A,
+	GenericLagrangianForm::GenericLagrangianForm(const StiffnessMatrix &A,
 												 const Eigen::VectorXd &b)
-		: AugmentedLagrangianForm(constraint_nodes), A(A), b(b)
+		: A(A), b(b)
 	{
 	}
 
 	double GenericLagrangianForm::value_unweighted(const Eigen::VectorXd &x) const
 	{
 		const Eigen::VectorXd res = A * x - b;
-		const double L_penalty = lagr_mults_.transpose() * res;
-		const double A_penalty = res.squaredNorm() / 2;
-		return L_penalty + k_al_ * A_penalty;
+		const double AL_penalty = lagr_mults_.transpose() * res;
+		return AL_penalty;
 	}
 
 	void GenericLagrangianForm::first_derivative_unweighted(const Eigen::VectorXd &x, Eigen::VectorXd &gradv) const
 	{
-		gradv = A * lagr_mults_ + k_al_ * (A * x - b);
+		gradv = A * lagr_mults_;
 	}
 
 	void GenericLagrangianForm::second_derivative_unweighted(const Eigen::VectorXd &x, StiffnessMatrix &hessian) const
 	{
-		hessian = A;
+		hessian.resize(A.rows(), A.cols());
+		hessian.setZero();
 	}
 
 	void GenericLagrangianForm::update_lagrangian(const Eigen::VectorXd &x, const double k_al)
 	{
 		lagr_mults_ += k_al * (A * x - b);
-	}
-
-	double GenericLagrangianForm::compute_error(const Eigen::VectorXd &x) const
-	{
-		const Eigen::VectorXd res = A * x - b;
-		return res.norm();
 	}
 } // namespace polyfem::solver
