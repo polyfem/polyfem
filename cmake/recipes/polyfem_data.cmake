760210f4eedafb2237ab5306d36e481f35882c6d
maxpaik16 <maxpaik@gmail.com>
2024-11-18 09:31:37 -0500

    update data repo

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -28,7 +28,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/maxpaik16/polyfem-data
-        GIT_TAG f3ae51882acbec37252153f8aa6a57a193c63204
+        GIT_TAG fa77c81b3efb74dddf80ed7c66d1a0e2180b40fd
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
