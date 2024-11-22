8637b3f89c8f3ed16eafe2a27fd2d247319df7a3
ZizhouHuang <zizhou@nyu.edu>
2024-11-22 14:15:31 -0500

    update data

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -28,7 +28,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-        GIT_TAG 58e559152b03f8539798fa2679829c3a759cda3a
+        GIT_TAG 75a2b4067ad6896e4b9238fe3d8e6ccf1eb5be34
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
