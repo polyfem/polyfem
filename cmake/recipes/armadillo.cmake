8079cc8e891a8b35cb2a419c086758db2e65d15c
teseo <teseo.schneider@gmail.com>
2025-04-21 11:28:04 -0700

    disabled smoke test from armadillo

--- cmake/recipes/armadillo.cmake
+++ cmake/recipes/armadillo.cmake
@@ -7,6 +7,8 @@ endif()
 
 message(STATUS "Third-party: creating target 'armadillo::armadillo'")
 
+set(BUILD_SMOKE_TEST OFF CACHE STRING "Build smoke tests" FORCE)
+
 include(CPM)
 CPMAddPackage("gl:conradsnicta/armadillo-code#3a23ca275b9fe30a7ef3132fc9c91c4b2ab29f9d")
 add_library(armadillo::armadillo ALIAS armadillo)
