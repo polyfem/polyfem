0b33a7d4e3aae200db47c6f46696f8a8a8489e91
teseoch <teseo.schneider@gmail.com>
2025-05-16 16:19:31 -0700

    commented not compiling assert

--- src/polyfem/solver/NLProblem.cpp
+++ src/polyfem/solver/NLProblem.cpp
@@ -332,7 +332,7 @@ namespace polyfem::solver
 		assert(test1.nonZeros() != 0);
 #endif
 
-		assert((Q1_ * R1_ - At * P_).norm() < 1e-10);
+		// assert((Q1_ * R1_ - At * P_).norm() < 1e-10);
 
 		std::vector<std::shared_ptr<Form>> tmp;
 		tmp.insert(tmp.end(), penalty_forms_.begin(), penalty_forms_.end());
