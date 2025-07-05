dbb2bf513e55aed64e0c3209ab232ae9ae4c5b7e
ZizhouHuang <zizhou@nyu.edu>
2025-07-05 11:12:07 -0700

    Update toolkit

--- cmake/recipes/ipc_toolkit.cmake
+++ cmake/recipes/ipc_toolkit.cmake
@@ -8,4 +8,4 @@ endif()
 message(STATUS "Third-party: creating target 'ipc::toolkit'")
 
 include(CPM)
-CPMAddPackage("gh:geometryprocessing/GCP-toolkit#ae528e7d7dcc7e3d81d5190db2147f7340454f2a")
+CPMAddPackage("gh:geometryprocessing/GCP-toolkit#7185ecd8b7b46aadae78db688c6029a82a21dcca")
