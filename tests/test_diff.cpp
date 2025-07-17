4b0b9df50bb396abaaecbb0060792a6c6300faf0
teseoch <teseo.schneider@gmail.com>
2025-07-17 13:25:22 -0700

    adding root to adjoint

--- tests/test_diff.cpp
+++ tests/test_diff.cpp
@@ -147,7 +147,7 @@ namespace
 		for (auto &arg : opt_args["states"])
 			arg["path"] = append_root_path(arg["path"]);
 
-		std::vector<std::shared_ptr<State>> states = AdjointOptUtils::create_states(opt_args["states"], solver::CacheLevel::Derivatives, 16);
+		std::vector<std::shared_ptr<State>> states = AdjointOptUtils::create_states("", opt_args["states"], solver::CacheLevel::Derivatives, 16);
 
 		/* DOF */
 		int ndof = 0;
@@ -1289,7 +1289,7 @@ TEST_CASE("initial-contact-smooth", "[test_adjoint]")
 	json obj_args = R"({
             "type": "transient_integral",
             "state": 0,
-            "static_objective": 
+            "static_objective":
             {
                 "type": "smooth_contact_force_norm",
                 "state": 0,
