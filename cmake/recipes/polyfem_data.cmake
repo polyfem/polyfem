9104af84aa0b0f023c459764094daf9de83bd5f2
teseoch <teseo.schneider@gmail.com>
2025-10-03 10:02:32 -0700

    new isochoric test

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -28,7 +28,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-        GIT_TAG b9d6fa398a8ac6aabb1ed8e5d8f6e655ead59b3d
+        GIT_TAG cacc50400372de1fb8f34f635ffbb6dea84105a0
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
