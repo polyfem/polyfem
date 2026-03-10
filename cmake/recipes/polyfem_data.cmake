9ac98daab839af41935860a04f00b470d63fcac5
teseoch <teseo.schneider@gmail.com>
2026-03-10 13:55:55 -0700

    more margin

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -28,7 +28,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-        GIT_TAG 6cda7bf0694c1c29a3afd023fa4466dbb9d6a34f
+        GIT_TAG 88932f3febc70fe85fa3736606fe84f2d27de909
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
