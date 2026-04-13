49a08df66e044aa6eeb6ec7a0e54f6f518d414fe
maxpaik16 <maxpaik@gmail.com>
2026-04-13 16:23:51 -0400

    update polysolve

--- cmake/recipes/polysolve.cmake
+++ cmake/recipes/polysolve.cmake
@@ -8,4 +8,4 @@ endif()
 message(STATUS "Third-party: creating target 'polysolve'")
 
 include(CPM)
-CPMAddPackage("gh:maxpaik16/polysolve#2dfa01ea5a0e149c2725270b3d95982773f8947d")
+CPMAddPackage("gh:maxpaik16/polysolve#65fe6a6d8ef7a1c7f8b6aff348784d1cff69e4cb")
