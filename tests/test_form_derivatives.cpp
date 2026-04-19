2e58408be095bcdc4b6e76dd35361c00d835b10e
Zizhou Huang <zizhou@nyu.edu>
2026-04-18 20:49:49 -0700

    json option barrier type

--- tests/test_form_derivatives.cpp
+++ tests/test_form_derivatives.cpp
@@ -428,7 +428,7 @@ TEST_CASE("high-order contact friction form derivatives", "[form][form_derivativ
 	const bool use_adaptive_barrier_stiffness = false;
 	const double ccd_tolerance = 1e-6;
 	const int ccd_max_iterations = static_cast<int>(1e6);
-	const json ho_params = json::object({{"quadrature_order", 5}, {"dbar_factor", 1.0}, {"exponent", 0}, {"normalize_weights", false}});
+	const json ho_params = json::object({{"quadrature_order", 5}, {"dbar_factor", 1.0}, {"normalize_weights", false}});
 
 	const HighOrderContactForm contact_form(
 		state_ptr->collision_mesh, dhat, state_ptr->avg_mass, ho_params, /*skip_obstacles=*/false,
