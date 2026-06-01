d652a1ff8d37ad383005302b6210ff92de197dc6
Ian Su <TzuHsiangSu3000@gmail.com>
2026-06-01 14:05:02 -0400

    exp-policy-fix

--- cmake/recipes/paraviewo.cmake
+++ cmake/recipes/paraviewo.cmake
@@ -8,4 +8,4 @@ endif()
 message(STATUS "Third-party: creating target 'paraviewo::paraviewo'")
 
 include(CPM)
-CPMAddPackage("gh:polyfem/paraviewo#50e75b3d408fb1bb5d46cadc2126a5a71101c5e4")
+CPMAddPackage("gh:polyfem/paraviewo#7af5fc9a2c5c28c7ab7107964faf25ba20f1cdae")
