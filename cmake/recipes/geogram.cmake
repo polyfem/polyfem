808b6f01a5a463b144cc691558fe4a7ba752399c
Ian Su <TzuHsiangSu3000@gmail.com>
2026-06-01 12:58:39 -0400

    update geogram and paraviewo

--- cmake/recipes/geogram.cmake
+++ cmake/recipes/geogram.cmake
@@ -38,7 +38,7 @@ endif()
 ################################################################################
 
 include(CPM)
-CPMAddPackage("gh:polyfem/geogram#3191ff581d4087ebddff735a21bf3ca2415f53be")
+CPMAddPackage("gh:polyfem/geogram#d38b11a32dd68e23db7a97dd7de31b43478bff2d")
 
 find_path(GEOGRAM_SOURCE_INCLUDE_DIR
 		geogram/basic/common.h
--- cmake/recipes/paraviewo.cmake
+++ cmake/recipes/paraviewo.cmake
@@ -8,4 +8,4 @@ endif()
 message(STATUS "Third-party: creating target 'paraviewo::paraviewo'")
 
 include(CPM)
-CPMAddPackage("gh:polyfem/paraviewo#9ac491eb3a2a28b6725bf252ac3bf47c78882fb8")
+CPMAddPackage("gh:polyfem/paraviewo#50e75b3d408fb1bb5d46cadc2126a5a71101c5e4")
