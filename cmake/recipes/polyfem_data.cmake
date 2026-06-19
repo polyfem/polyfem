4223e940d3bea17d9f25d1981cc1e490b84719b6
teseoch <teseo.schneider@gmail.com>
2026-06-19 16:04:08 -0700

    updated data

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -28,7 +28,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-        GIT_TAG 1a63bf4f0fa8c489cd3c9fc0a7fd73dc180affde
+        GIT_TAG 88ecd9aa9108bf0185dfbfa3de7e76bedddd7e26
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
