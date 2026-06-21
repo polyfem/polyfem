68da782b8f5738bf0faa54f972e291caf870cb26
teseo <teseo.schneider@gmail.com>
2026-06-20 18:48:17 -0700

    fixing jsons

--- cmake/recipes/polyfem_data.cmake
+++ cmake/recipes/polyfem_data.cmake
@@ -28,7 +28,7 @@ else()
         PREFIX ${FETCHCONTENT_BASE_DIR}/polyfem-test-data
         SOURCE_DIR ${POLYFEM_DATA_DIR}
         GIT_REPOSITORY https://github.com/polyfem/polyfem-data
-        GIT_TAG 88ecd9aa9108bf0185dfbfa3de7e76bedddd7e26
+        GIT_TAG d783af71c596a2a607e67c9ea7d07cce48b96a55
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
